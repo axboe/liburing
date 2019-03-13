axboe/liburing

commit 8260029608b9650c9d230e9668935de7862dc845

queue: ensure io_uring_submit() returns the right error
We weren't passing back -errno for the system call failure.
This meant any error got turned into EPERM as far as the
caller was concerned.

Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/queue.c
@@ -81,6 +81,7 @@ int io_uring_submit(struct io_uring *ring)
 	struct io_uring_sq *sq = &ring->sq;
 	const unsigned mask = *sq->kring_mask;
 	unsigned ktail, ktail_next, submitted;
+	int ret;
 
 	/*
 	 * If we have pending IO in the kring, submit it first. We need a
@@ -132,8 +133,12 @@ int io_uring_submit(struct io_uring *ring)
 	}
 
 submit:
-	return io_uring_enter(ring->ring_fd, submitted, 0,
+	ret = io_uring_enter(ring->ring_fd, submitted, 0,
 				IORING_ENTER_GETEVENTS, NULL);
+	if (ret < 0)
+		return -errno;
+
+	return 0;
 }
 
 /*

commit a86522197d041b889519d6744d52477e80acce1e

queue: ensure io_uring_submit() returns the system call value
A previous fix that ensured we pass back the right error messed
up the normal return, which is number of entries submitted.
This makes the poll test cases fail.

Fixes: 8260029608b9 ("queue: ensure io_uring_submit() returns the right error")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/queue.c
@@ -138,7 +138,7 @@ submit:
 	if (ret < 0)
 		return -errno;
 
-	return 0;
+	return ret;
 }
 
 /*