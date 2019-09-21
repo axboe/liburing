axboe/liburing

commit 76e9232baa006c56d779b568dd0b6ec3862d4107

Add io_uring_wait_cqe_timeout()
Like io_uring_wait_cqe(), except it accepts a timeout value as well. Note
that an sqe is used internally to handle the timeout. Applications using
this function must never set sqe->user_data to LIBURING_UDATA_TIMEOUT!

Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/queue.c
@@ -12,25 +12,35 @@
 #include "liburing/barrier.h"
 
 static int __io_uring_get_cqe(struct io_uring *ring,
-			      struct io_uring_cqe **cqe_ptr, int wait)
+			      struct io_uring_cqe **cqe_ptr, unsigned submit,
+			      int wait)
 {
+	int ret, err = 0;
 	unsigned head;
-	int ret;
 
 	do {
 		io_uring_for_each_cqe(ring, head, *cqe_ptr)
 			break;
-		if (*cqe_ptr)
+		if (*cqe_ptr) {
+			if ((*cqe_ptr)->user_data == LIBURING_UDATA_TIMEOUT) {
+				if ((*cqe_ptr)->res < 0)
+					err = (*cqe_ptr)->res;
+				io_uring_cq_advance(ring, 1);
+				if (!err)
+					continue;
+				*cqe_ptr = NULL;
+			}
 			break;
+		}
 		if (!wait)
 			return -EAGAIN;
-		ret = io_uring_enter(ring->ring_fd, 0, 1,
-					IORING_ENTER_GETEVENTS, NULL);
+		ret = io_uring_enter(ring->ring_fd, submit, 1,
+				IORING_ENTER_GETEVENTS, NULL);
 		if (ret < 0)
 			return -errno;
 	} while (1);
 
-	return 0;
+	return err;
 }
 
 /*
@@ -39,7 +49,7 @@ static int __io_uring_get_cqe(struct io_uring *ring,
  */
 int io_uring_peek_cqe(struct io_uring *ring, struct io_uring_cqe **cqe_ptr)
 {
-	return __io_uring_get_cqe(ring, cqe_ptr, 0);
+	return __io_uring_get_cqe(ring, cqe_ptr, 0, 0);
 }
 
 /*
@@ -48,7 +58,38 @@ int io_uring_peek_cqe(struct io_uring *ring, struct io_uring_cqe **cqe_ptr)
  */
 int io_uring_wait_cqe(struct io_uring *ring, struct io_uring_cqe **cqe_ptr)
 {
-	return __io_uring_get_cqe(ring, cqe_ptr, 1);
+	return __io_uring_get_cqe(ring, cqe_ptr, 0, 1);
+}
+
+/*
+ * Like io_uring_wait_cqe(), except it accepts a timeout value as well. Note
+ * that an sqe is used internally to handle the timeout. Applications using
+ * this function must never set sqe->user_data to LIBURING_UDATA_TIMEOUT!
+ */
+int io_uring_wait_cqe_timeout(struct io_uring *ring,
+			      struct io_uring_cqe **cqe_ptr,
+			      struct timespec *ts)
+{
+	struct io_uring_sqe *sqe;
+	int ret;
+
+	/*
+	 * If the SQ ring is full, we may need to submit IO first
+	 */
+	sqe = io_uring_get_sqe(ring);
+	if (!sqe) {
+		ret = io_uring_submit(ring);
+		if (ret)
+			return ret;
+		sqe = io_uring_get_sqe(ring);
+	}
+	io_uring_prep_timeout(sqe, ts, 1);
+	sqe->user_data = LIBURING_UDATA_TIMEOUT;
+	ret = io_uring_submit(ring);
+	if (ret)
+		return ret;
+
+	return __io_uring_get_cqe(ring, cqe_ptr, 1, 1);
 }
 
 /*

src/include/liburing.h
@@ -74,6 +74,8 @@ extern int io_uring_peek_cqe(struct io_uring *ring,
 	struct io_uring_cqe **cqe_ptr);
 extern int io_uring_wait_cqe(struct io_uring *ring,
 	struct io_uring_cqe **cqe_ptr);
+extern int io_uring_wait_cqe_timeout(struct io_uring *ring,
+	struct io_uring_cqe **cqe_ptr, struct timespec *ts);
 extern int io_uring_submit(struct io_uring *ring);
 extern int io_uring_submit_and_wait(struct io_uring *ring, unsigned wait_nr);
 extern struct io_uring_sqe *io_uring_get_sqe(struct io_uring *ring);
@@ -87,6 +89,8 @@ extern int io_uring_register_files(struct io_uring *ring, const int *files,
 extern int io_uring_unregister_files(struct io_uring *ring);
 extern int io_uring_register_eventfd(struct io_uring *ring, int fd);
 extern int io_uring_unregister_eventfd(struct io_uring *ring);
+
+#define LIBURING_UDATA_TIMEOUT	((__u64) -1)
 
 #define io_uring_for_each_cqe(ring, head, cqe)					\
 	/*									\

src/liburing.map
@@ -5,6 +5,7 @@ LIBURING_0.1 {
 		io_uring_queue_exit;
 		io_uring_peek_cqe;
 		io_uring_wait_cqe;
+		io_uring_wait_cqe_timeout;
 		io_uring_submit;
 		io_uring_submit_and_wait;
 		io_uring_get_sqe;

test/timeout.c
@@ -177,6 +177,57 @@ err:
 	return 1;
 }
 
+static int test_single_timeout_wait(struct io_uring *ring)
+{
+	struct io_uring_cqe *cqe;
+	struct io_uring_sqe *sqe;
+	struct timespec ts;
+	int i, ret;
+
+	sqe = io_uring_get_sqe(ring);
+	io_uring_prep_nop(sqe);
+	io_uring_sqe_set_data(sqe, (void *) 1);
+
+	sqe = io_uring_get_sqe(ring);
+	io_uring_prep_nop(sqe);
+	io_uring_sqe_set_data(sqe, (void *) 1);
+
+	ret = io_uring_submit(ring);
+	if (ret <= 0) {
+		printf("sqe submit failed: %d\n", ret);
+		goto err;
+	}
+
+	ts.tv_sec = 1;
+	ts.tv_nsec = 0;
+
+	i = 0;
+	do {
+		ret = io_uring_wait_cqe_timeout(ring, &cqe, &ts);
+		if (ret == -ETIME)
+			break;
+		if (ret < 0) {
+			printf("wait timeout failed: %d\n", ret);
+			goto err;
+		}
+
+		if (cqe->res < 0) {
+			printf("res: %d\n", cqe->res);
+			goto err;
+		}
+		io_uring_cqe_seen(ring, cqe);
+		i++;
+	} while (1);
+
+	if (i != 2) {
+		printf("got %d completions\n", i);
+		goto err;
+	}
+	return 0;
+err:
+	return 1;
+}
+
 /*
  * Test single timeout waking us up
  */
@@ -290,6 +341,12 @@ int main(int argc, char *argv[])
 	ret = test_single_timeout_nr(&ring);
 	if (ret) {
 		printf("test_single_timeout_nr failed\n");
+		return ret;
+	}
+
+	ret = test_single_timeout_wait(&ring);
+	if (ret) {
+		printf("test_single_timeout_wait failed\n");
 		return ret;
 	}
 

commit 5652c3bcda738c16ab4e6f12e637559cc379a4e7

io_uring_wait_cqe_timeout: fix wrong error return check
Fixes: 76e9232baa00 ("Add io_uring_wait_cqe_timeout()")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/queue.c
@@ -79,14 +79,14 @@ int io_uring_wait_cqe_timeout(struct io_uring *ring,
 	sqe = io_uring_get_sqe(ring);
 	if (!sqe) {
 		ret = io_uring_submit(ring);
-		if (ret)
+		if (ret < 0)
 			return ret;
 		sqe = io_uring_get_sqe(ring);
 	}
 	io_uring_prep_timeout(sqe, ts, 1);
 	sqe->user_data = LIBURING_UDATA_TIMEOUT;
 	ret = io_uring_submit(ring);
-	if (ret)
+	if (ret < 0)
 		return ret;
 
 	return __io_uring_get_cqe(ring, cqe_ptr, 1, 1);