axboe/liburing

commit 20c929379461d790fb7556a175e12d719bcd20b7

Inline fast-path of io_uring_{wait,peek}_cqe()
For cases where we usually have completions available, it can be fairly
costly to always have to call into the liburing library. Ensure that we
handle the fast path of finding completions without needing a library
call.

Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/include/liburing.h
@@ -70,12 +70,8 @@ extern int io_uring_queue_init(unsigned entries, struct io_uring *ring,
 extern int io_uring_queue_mmap(int fd, struct io_uring_params *p,
 	struct io_uring *ring);
 extern void io_uring_queue_exit(struct io_uring *ring);
-extern int io_uring_peek_cqe(struct io_uring *ring,
-	struct io_uring_cqe **cqe_ptr);
 unsigned io_uring_peek_batch_cqe(struct io_uring *ring,
 	struct io_uring_cqe **cqes, unsigned count);
-extern int io_uring_wait_cqe(struct io_uring *ring,
-	struct io_uring_cqe **cqe_ptr);
 extern int io_uring_wait_cqes(struct io_uring *ring,
 	struct io_uring_cqe **cqe_ptr, unsigned wait_nr, struct timespec *ts,
 	sigset_t *sigmask);
@@ -94,6 +90,14 @@ extern int io_uring_register_files(struct io_uring *ring, const int *files,
 extern int io_uring_unregister_files(struct io_uring *ring);
 extern int io_uring_register_eventfd(struct io_uring *ring, int fd);
 extern int io_uring_unregister_eventfd(struct io_uring *ring);
+
+/*
+ * Helper for the peek/wait single cqe functions. Exported because of that,
+ * but probably shouldn't be used directly in an application.
+ */
+extern int __io_uring_get_cqe(struct io_uring *ring,
+			      struct io_uring_cqe **cqe_ptr, unsigned submit,
+			      unsigned wait_nr, sigset_t *sigmask);
 
 #define LIBURING_UDATA_TIMEOUT	((__u64) -1)
 
@@ -254,6 +258,59 @@ static inline unsigned io_uring_sq_space_left(struct io_uring *ring)
 static inline unsigned io_uring_cq_ready(struct io_uring *ring)
 {
 	return io_uring_smp_load_acquire(ring->cq.ktail) - *ring->cq.khead;
+}
+
+static struct io_uring_cqe *__io_uring_peek_cqe(struct io_uring *ring)
+{
+	struct io_uring_cqe *cqe;
+	unsigned head;
+	int err = 0;
+
+	do {
+		io_uring_for_each_cqe(ring, head, cqe)
+			break;
+		if (cqe) {
+			if (cqe->user_data == LIBURING_UDATA_TIMEOUT) {
+				if (cqe->res < 0)
+					err = cqe->res;
+				io_uring_cq_advance(ring, 1);
+				if (!err)
+					continue;
+				cqe = NULL;
+			}
+		}
+		break;
+	} while (1);
+
+	return cqe;
+}
+
+/*
+ * Return an IO completion, if one is readily available. Returns 0 with
+ * cqe_ptr filled in on success, -errno on failure.
+ */
+static inline int io_uring_peek_cqe(struct io_uring *ring,
+				    struct io_uring_cqe **cqe_ptr)
+{
+	*cqe_ptr = __io_uring_peek_cqe(ring);
+	if (*cqe_ptr)
+		return 0;
+
+	return __io_uring_get_cqe(ring, cqe_ptr, 0, 0, NULL);
+}
+
+/*
+ * Return an IO completion, waiting for it if necessary. Returns 0 with
+ * cqe_ptr filled in on success, -errno on failure.
+ */
+static inline int io_uring_wait_cqe(struct io_uring *ring,
+				    struct io_uring_cqe **cqe_ptr)
+{
+	*cqe_ptr = __io_uring_peek_cqe(ring);
+	if (*cqe_ptr)
+		return 0;
+
+	return __io_uring_get_cqe(ring, cqe_ptr, 0, 1, NULL);
 }
 
 #ifdef __cplusplus

src/queue.c
@@ -11,45 +11,26 @@
 #include "liburing.h"
 #include "liburing/barrier.h"
 
-static int __io_uring_get_cqe(struct io_uring *ring,
-			      struct io_uring_cqe **cqe_ptr, unsigned submit,
-			      unsigned wait_nr, sigset_t *sigmask)
+int __io_uring_get_cqe(struct io_uring *ring, struct io_uring_cqe **cqe_ptr,
+		       unsigned submit, unsigned wait_nr, sigset_t *sigmask)
 {
 	int ret, err = 0;
-	unsigned head;
 
 	do {
-		io_uring_for_each_cqe(ring, head, *cqe_ptr)
+		*cqe_ptr = __io_uring_peek_cqe(ring);
+		if (*cqe_ptr)
 			break;
-		if (*cqe_ptr) {
-			if ((*cqe_ptr)->user_data == LIBURING_UDATA_TIMEOUT) {
-				if ((*cqe_ptr)->res < 0)
-					err = (*cqe_ptr)->res;
-				io_uring_cq_advance(ring, 1);
-				if (!err)
-					continue;
-				*cqe_ptr = NULL;
-			}
+		if (!wait_nr) {
+			err = -EAGAIN;
 			break;
 		}
-		if (!wait_nr)
-			return -EAGAIN;
 		ret = io_uring_enter(ring->ring_fd, submit, wait_nr,
-				IORING_ENTER_GETEVENTS, sigmask);
+					IORING_ENTER_GETEVENTS, sigmask);
 		if (ret < 0)
-			return -errno;
-	} while (1);
+			err = -errno;
+	} while (!err);
 
 	return err;
-}
-
-/*
- * Return an IO completion, if one is readily available. Returns 0 with
- * cqe_ptr filled in on success, -errno on failure.
- */
-int io_uring_peek_cqe(struct io_uring *ring, struct io_uring_cqe **cqe_ptr)
-{
-	return __io_uring_get_cqe(ring, cqe_ptr, 0, 0, NULL);
 }
 
 /*
@@ -112,15 +93,6 @@ static int __io_uring_flush_sq(struct io_uring *ring)
 		io_uring_smp_store_release(sq->ktail, ktail);
 
 	return submitted;
-}
-
-/*
- * Return an IO completion, waiting for it if necessary. Returns 0 with
- * cqe_ptr filled in on success, -errno on failure.
- */
-int io_uring_wait_cqe(struct io_uring *ring, struct io_uring_cqe **cqe_ptr)
-{
-	return __io_uring_get_cqe(ring, cqe_ptr, 0, 1, NULL);
 }
 
 /*

src/liburing.map
@@ -28,4 +28,6 @@ LIBURING_0.2 {
 		io_uring_peek_batch_cqe;
 		io_uring_wait_cqe_timeout;
 		io_uring_wait_cqes;
+
+		__io_uring_get_cqe;
 } LIBURING_0.1;

commit f6c8044fd0d4fb8b8b2136d3329b38576a706308

Fix timeout detection
We lost the -ETIME return from a previous commit that optimized the
exported peek/wait cqe path, make sure that it works again.

Fixes: 20c929379461 ("Inline fast-path of io_uring_{wait,peek}_cqe()")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/include/liburing.h
@@ -260,7 +260,7 @@ static inline unsigned io_uring_cq_ready(struct io_uring *ring)
 	return io_uring_smp_load_acquire(ring->cq.ktail) - *ring->cq.khead;
 }
 
-static struct io_uring_cqe *__io_uring_peek_cqe(struct io_uring *ring)
+static int __io_uring_peek_cqe(struct io_uring *ring, struct io_uring_cqe **cqe_ptr)
 {
 	struct io_uring_cqe *cqe;
 	unsigned head;
@@ -282,7 +282,8 @@ static struct io_uring_cqe *__io_uring_peek_cqe(struct io_uring *ring)
 		break;
 	} while (1);
 
-	return cqe;
+	*cqe_ptr = cqe;
+	return err;
 }
 
 /*
@@ -292,9 +293,11 @@ static struct io_uring_cqe *__io_uring_peek_cqe(struct io_uring *ring)
 static inline int io_uring_peek_cqe(struct io_uring *ring,
 				    struct io_uring_cqe **cqe_ptr)
 {
-	*cqe_ptr = __io_uring_peek_cqe(ring);
-	if (*cqe_ptr)
-		return 0;
+	int err;
+
+	err = __io_uring_peek_cqe(ring, cqe_ptr);
+	if (err)
+		return err;
 
 	return __io_uring_get_cqe(ring, cqe_ptr, 0, 0, NULL);
 }
@@ -306,9 +309,11 @@ static inline int io_uring_peek_cqe(struct io_uring *ring,
 static inline int io_uring_wait_cqe(struct io_uring *ring,
 				    struct io_uring_cqe **cqe_ptr)
 {
-	*cqe_ptr = __io_uring_peek_cqe(ring);
-	if (*cqe_ptr)
-		return 0;
+	int err;
+
+	err = __io_uring_peek_cqe(ring, cqe_ptr);
+	if (err)
+		return err;
 
 	return __io_uring_get_cqe(ring, cqe_ptr, 0, 1, NULL);
 }

src/queue.c
@@ -17,8 +17,8 @@ int __io_uring_get_cqe(struct io_uring *ring, struct io_uring_cqe **cqe_ptr,
 	int ret, err = 0;
 
 	do {
-		*cqe_ptr = __io_uring_peek_cqe(ring);
-		if (*cqe_ptr)
+		err = __io_uring_peek_cqe(ring, cqe_ptr);
+		if (err || *cqe_ptr)
 			break;
 		if (!wait_nr) {
 			err = -EAGAIN;
@@ -28,6 +28,7 @@ int __io_uring_get_cqe(struct io_uring *ring, struct io_uring_cqe **cqe_ptr,
 					IORING_ENTER_GETEVENTS, sigmask);
 		if (ret < 0)
 			err = -errno;
+		submit -= ret;
 	} while (!err);
 
 	return err;