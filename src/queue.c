4fcf0777c4c334d0b785baed0ec72411905001e9
Kornilios Kourtis <kkourt@kkourt.io>
2019-10-30 16:07:53 +0100

    sq_ring_needs_enter(): force loading of kflags
    ring->sq.kflags is set by the kernel. Ensure the compiler always loads
    the value from memory when doing the check.
    
    Signed-off-by: Kornilios Kourtis <kornilios@gmail.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- src/queue.c
+++ src/queue.c
@@ -152,7 +152,7 @@ static inline bool sq_ring_needs_enter(struct io_uring *ring, unsigned *flags)
 {
 	if (!(ring->flags & IORING_SETUP_SQPOLL))
 		return true;
-	if ((*ring->sq.kflags & IORING_SQ_NEED_WAKEUP)) {
+	if (IO_URING_READ_ONCE(*ring->sq.kflags) & IORING_SQ_NEED_WAKEUP) {
 		*flags |= IORING_ENTER_SQ_WAKEUP;
 		return true;
 	}
