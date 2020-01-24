69e4cf92793e25ee21299aa0efff317894a6904a
Jens Axboe <axboe@kernel.dk>
2020-01-24 10:00:17 -0700

    IORING_SETUP_SHARED got renamed to IORING_SETUP_ATTACH_WQ
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- src/include/liburing/io_uring.h
+++ src/include/liburing/io_uring.h
@@ -75,7 +75,7 @@ enum {
 #define IORING_SETUP_SQ_AFF	(1U << 2)	/* sq_thread_cpu is valid */
 #define IORING_SETUP_CQSIZE	(1U << 3)	/* app defines CQ size */
 #define IORING_SETUP_CLAMP	(1U << 4)	/* clamp SQ/CQ ring sizes */
-#define IORING_SETUP_SHARED	(1U << 5)	/* share workqueue backend */
+#define IORING_SETUP_ATTACH_WQ	(1U << 5)	/* attach to existing wq */
 
 enum {
 	IORING_OP_NOP,
