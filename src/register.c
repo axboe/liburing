axboe/liburing

commit ec0ce01687421557736b3b7a05d002ea122fc86f

Add io_uring_register_probe()
Helper to return a struct of supported op codes.

Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/register.c
@@ -98,12 +98,13 @@ int io_uring_register_eventfd(struct io_uring *ring, int event_fd)
 	return 0;
 }
 
-int io_uring_unregister_eventfd(struct io_uring *ring)
+int io_uring_register_probe(struct io_uring *ring, struct io_uring_probe *p,
+			    unsigned int nr_ops)
 {
 	int ret;
 
-	ret = __sys_io_uring_register(ring->ring_fd, IORING_UNREGISTER_EVENTFD,
-					NULL, 0);
+	ret = __sys_io_uring_register(ring->ring_fd, IORING_REGISTER_PROBE,
+					p, nr_ops);
 	if (ret < 0)
 		return -errno;
 

src/include/liburing.h
@@ -102,6 +102,8 @@ extern int io_uring_register_files_update(struct io_uring *ring, unsigned off,
 					int *files, unsigned nr_files);
 extern int io_uring_register_eventfd(struct io_uring *ring, int fd);
 extern int io_uring_unregister_eventfd(struct io_uring *ring);
+extern int io_uring_register_probe(struct io_uring *ring,
+					struct io_uring_probe *p, unsigned nr);
 
 /*
  * Helper for the peek/wait single cqe functions. Exported because of that,

src/liburing.map
@@ -69,4 +69,5 @@ LIBURING_0.3 {
 LIBURING_0.4 {
 	global:
 		io_uring_ring_dontfork;
+		io_uring_register_probe;
 } LIBURING_0.3;

commit 59d0acf1c21cac6f3b46fd8243849982310e4195

Reinstate io_uring_unregister_eventfd()
When the probe registration was added, this got inadvertently
deleted. Oops...

Fixes: ec0ce0168742 ("Add io_uring_register_probe()")
Fixes: https://github.com/axboe/liburing/issues/86
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/register.c
@@ -99,6 +99,18 @@ int io_uring_register_eventfd(struct io_uring *ring, int event_fd)
 	return 0;
 }
 
+int io_uring_unregister_eventfd(struct io_uring *ring)
+{
+	int ret;
+
+	ret = __sys_io_uring_register(ring->ring_fd, IORING_UNREGISTER_EVENTFD,
+					NULL, 0);
+	if (ret < 0)
+		return -errno;
+
+	return 0;
+}
+
 int io_uring_register_probe(struct io_uring *ring, struct io_uring_probe *p,
 			    unsigned int nr_ops)
 {