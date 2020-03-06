4f48c04e85ba43cba8af7e96ecf9bed89856a6ab
Jens Axboe <axboe@kernel.dk>
2020-03-06 07:03:24 -0700

    io_uring_wait_cqes(): correct comment
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- src/queue.c
+++ src/queue.c
@@ -138,11 +138,11 @@ out:
  * that an sqe is used internally to handle the timeout. Applications using
  * this function must never set sqe->user_data to LIBURING_UDATA_TIMEOUT!
  *
- * Note that the application need not call io_uring_submit() before calling
- * this function, as we will do that on its behalf. From this it also follows
- * that this function isn't safe to use for applications that split SQ and CQ
- * handling between two threads and expect that to work without synchronization,
- * as this function manipulates both the SQ and CQ side.
+ * If 'ts' is specified, the application need not call io_uring_submit() before
+ * calling this function, as we will do that on its behalf. From this it also
+ * follows that this function isn't safe to use for applications that split SQ
+ * and CQ handling between two threads and expect that to work without
+ * synchronization, as this function manipulates both the SQ and CQ side.
  */
 int io_uring_wait_cqes(struct io_uring *ring, struct io_uring_cqe **cqe_ptr,
 		       unsigned wait_nr, struct __kernel_timespec *ts,
