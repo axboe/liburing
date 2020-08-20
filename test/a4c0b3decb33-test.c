515604dc3c5e7528df2325a0219af0bd394e4294
Jens Axboe <axboe@kernel.dk>
2020-08-20 05:38:50 -0600

    test/a4c0b3decb33-test: limit to 5000 iterations
    We currently rely on this test being killed by runtests.sh, and
    the test expects this and exits nicely when that happens. But let's
    give it a finite runtime in general, so that running it independently
    will have it exit nicely on its own as well.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- test/a4c0b3decb33-test.c
+++ test/a4c0b3decb33-test.c
@@ -106,7 +106,7 @@ static void execute_one(void);
 static void loop(void)
 {
 	int iter;
-	for (iter = 0;; iter++) {
+	for (iter = 0; iter < 5000; iter++) {
 		int pid = fork();
 		if (pid < 0)
 			exit(1);
