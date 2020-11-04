46e65de26e780ed84623338ba6652999c266bb80
Jens Axboe <axboe@kernel.dk>
2020-11-04 11:55:25 -0700

    Add __sys_io_uring_enter2()
    Just like __sys_io_uring_enter(), except it allows passing in the size
    of the last argument.
    
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- src/syscall.c
+++ src/syscall.c
@@ -48,9 +48,16 @@ int __sys_io_uring_setup(unsigned entries, struct io_uring_params *p)
 	return syscall(__NR_io_uring_setup, entries, p);
 }
 
+int __sys_io_uring_enter2(int fd, unsigned to_submit, unsigned min_complete,
+			 unsigned flags, sigset_t *sig, int sz)
+{
+	return syscall(__NR_io_uring_enter, fd, to_submit, min_complete,
+			flags, sig, sz);
+}
+
 int __sys_io_uring_enter(int fd, unsigned to_submit, unsigned min_complete,
 			 unsigned flags, sigset_t *sig)
 {
-	return syscall(__NR_io_uring_enter, fd, to_submit, min_complete,
-			flags, sig, _NSIG / 8);
+	return __sys_io_uring_enter2(fd, to_submit, min_complete, flags, sig,
+					_NSIG / 8);
 }
