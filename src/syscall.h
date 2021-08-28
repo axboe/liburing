fb1a8c1bb15ef73dee3b69b9e2ccfadb67a02e96
Jens Axboe <axboe@kernel.dk>
2021-08-28 15:06:52 -0600

    src/syscall.h: get rid of useless externs
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- src/syscall.h
+++ src/syscall.h
@@ -9,12 +9,12 @@ struct io_uring_params;
 /*
  * System calls
  */
-extern int __sys_io_uring_setup(unsigned entries, struct io_uring_params *p);
-extern int __sys_io_uring_enter(int fd, unsigned to_submit,
-	unsigned min_complete, unsigned flags, sigset_t *sig);
-extern int __sys_io_uring_enter2(int fd, unsigned to_submit,
-	unsigned min_complete, unsigned flags, sigset_t *sig, int sz);
-extern int __sys_io_uring_register(int fd, unsigned int opcode, const void *arg,
-	unsigned int nr_args);
+int __sys_io_uring_setup(unsigned entries, struct io_uring_params *p);
+int __sys_io_uring_enter(int fd, unsigned to_submit, unsigned min_complete,
+			 unsigned flags, sigset_t *sig);
+int __sys_io_uring_enter2(int fd, unsigned to_submit, unsigned min_complete,
+			  unsigned flags, sigset_t *sig, int sz);
+int __sys_io_uring_register(int fd, unsigned int opcode, const void *arg,
+			    unsigned int nr_args);
 
 #endif
