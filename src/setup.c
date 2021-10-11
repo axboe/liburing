fe927f867fd43f83d95de2482f8c9a1e99ec778f
Nursan Valeyev <tindarid@gmail.com>
2021-10-11 23:40:49 +0300

    setup.c: rewrite `__fls()`
    Rewrite handmade `__fls()`: use `__builtin_clz()`.
    Also add `inline` specifier.
    
    Signed-off-by: Nursan Valeyev <tindarid@gmail.com>

--- src/setup.c
+++ src/setup.c
@@ -211,33 +211,11 @@ void io_uring_free_probe(struct io_uring_probe *probe)
 	free(probe);
 }
 
-static int __fls(int x)
+static inline int __fls(int x)
 {
-	int r = 32;
-
 	if (!x)
 		return 0;
-	if (!(x & 0xffff0000u)) {
-		x <<= 16;
-		r -= 16;
-	}
-	if (!(x & 0xff000000u)) {
-		x <<= 8;
-		r -= 8;
-	}
-	if (!(x & 0xf0000000u)) {
-		x <<= 4;
-		r -= 4;
-	}
-	if (!(x & 0xc0000000u)) {
-		x <<= 2;
-		r -= 2;
-	}
-	if (!(x & 0x80000000u)) {
-		x <<= 1;
-		r -= 1;
-	}
-	return r;
+	return 8 * sizeof(x) - __builtin_clz(x);
 }
 
 static unsigned roundup_pow2(unsigned depth)
