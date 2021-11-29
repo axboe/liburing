2abb9a25e5aeb1adbe62642587dbbd0fb7fcfe27
Stefan Roesch <shr@fb.com>
2021-11-29 14:14:57 -0800

    liburing: Add helper functions for fgetxattr and getxattr
    Summary:
    
    This adds the helper functions for:
    - fgetxattr
    - getxattr
    
    Signed-off-by: Stefan Roesch <shr@fb.com>
    Link: https://lore.kernel.org/r/20211129221458.2546542-4-shr@fb.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- src/include/liburing.h
+++ src/include/liburing.h
@@ -708,6 +708,17 @@ static inline void io_uring_prep_getdents(struct io_uring_sqe *sqe, int fd,
 	io_uring_prep_rw(IORING_OP_GETDENTS, sqe, fd, buf, count, offset);
 }
 
+static inline void io_uring_prep_getxattr(struct io_uring_sqe *sqe,
+					  const char *name,
+					  const char *value,
+					  const char *path,
+					  size_t len)
+{
+	io_uring_prep_rw(IORING_OP_GETXATTR, sqe, 0, name, len, (__u64) value);
+	sqe->addr3 = (__u64) path;
+	sqe->xattr_flags = 0;
+}
+
 static inline void io_uring_prep_setxattr(struct io_uring_sqe *sqe,
 					  const char *name,
 					  const char *value,
@@ -718,6 +729,16 @@ static inline void io_uring_prep_setxattr(struct io_uring_sqe *sqe,
 	io_uring_prep_rw(IORING_OP_SETXATTR, sqe, 0, name, len, (__u64) value);
 	sqe->addr3 = (__u64) path;
 	sqe->xattr_flags = flags;
+}
+
+static inline void io_uring_prep_fgetxattr(struct io_uring_sqe *sqe,
+		                           int         fd,
+					   const char *name,
+					   const char *value,
+					   size_t      len)
+{
+	io_uring_prep_rw(IORING_OP_FGETXATTR, sqe, fd, name, len, (__u64) value);
+	sqe->xattr_flags = 0;
 }
 
 static inline void io_uring_prep_fsetxattr(struct io_uring_sqe *sqe,
