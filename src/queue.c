axboe/liburing

commit 504efe18c213dd753fe6c69afbfff6cfd50ba4cc

queue: avoid io_uring_enter() with no SQEs
io_uring_submit() makes an io_uring_enter() syscall even if no SQEs are
ready to submit. Move the empty SQ check from _io_uring_get_cqe() to
sq_ring_needs_enter() so io_uring_submit() can skip the syscall too.

Signed-off-by: Caleb Sander <csander@purestorage.com>

src/queue.c
@@ -12,9 +12,15 @@
  * Returns true if we're not using SQ thread (thus nobody submits but us)
  * or if IORING_SQ_NEED_WAKEUP is set, so submit thread must be explicitly
  * awakened. For the latter case, we set the thread wakeup flag.
+ * If no SQEs are ready for submission, returns false.
  */
-static inline bool sq_ring_needs_enter(struct io_uring *ring, unsigned *flags)
+static inline bool sq_ring_needs_enter(struct io_uring *ring,
+				       unsigned submit,
+				       unsigned *flags)
 {
+	if (!submit)
+		return false;
+
 	if (!(ring->flags & IORING_SETUP_SQPOLL))
 		return true;
 
@@ -85,7 +91,7 @@ static int _io_uring_get_cqe(struct io_uring *ring,
 			flags = IORING_ENTER_GETEVENTS | data->get_flags;
 			need_enter = true;
 		}
-		if (data->submit && sq_ring_needs_enter(ring, &flags))
+		if (sq_ring_needs_enter(ring, data->submit, &flags))
 			need_enter = true;
 		if (!need_enter)
 			break;
@@ -354,7 +360,7 @@ static int __io_uring_submit(struct io_uring *ring, unsigned submitted,
 	int ret;
 
 	flags = 0;
-	if (sq_ring_needs_enter(ring, &flags) || wait_nr) {
+	if (sq_ring_needs_enter(ring, submitted, &flags) || wait_nr) {
 		if (wait_nr || (ring->flags & IORING_SETUP_IOPOLL))
 			flags |= IORING_ENTER_GETEVENTS;
 		if (ring->int_flags & INT_FLAG_REG_RING)

commit e5297de76bdb1d3593ff947b0100eead52c9b148

queue: enter if CQ needs it, even if SQ doesn't
Even when not submitting SQEs, io_uring_enter()
may be needed for IOPOLL mode or if the CQ overflowed.
Let io_uring_submit() still make the syscall in this case.

Fixes: 504efe18c213 ("queue: avoid io_uring_enter() with no SQEs")
Signed-off-by: Caleb Sander <csander@purestorage.com>

src/queue.c
@@ -343,12 +343,13 @@ int io_uring_wait_cqe_timeout(struct io_uring *ring,
 static int __io_uring_submit(struct io_uring *ring, unsigned submitted,
 			     unsigned wait_nr)
 {
+	bool cq_needs_enter = wait_nr || cq_ring_needs_enter(ring);
 	unsigned flags;
 	int ret;
 
 	flags = 0;
-	if (sq_ring_needs_enter(ring, submitted, &flags) || wait_nr) {
-		if (wait_nr || (ring->flags & IORING_SETUP_IOPOLL))
+	if (sq_ring_needs_enter(ring, submitted, &flags) || cq_needs_enter) {
+		if (cq_needs_enter)
 			flags |= IORING_ENTER_GETEVENTS;
 		if (ring->int_flags & INT_FLAG_REG_RING)
 			flags |= IORING_ENTER_REGISTERED_RING;