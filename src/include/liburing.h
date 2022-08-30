f0b43c84cb3d1a4af4f1a32193bcad66fe458488
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2022-08-30 07:56:38 +0700

    syscall: Add io_uring syscall functions
    We have:
    
      man 2 io_uring_setup;
      man 2 io_uring_enter;
      man 2 io_uring_register;
    
    Those entries say that io_uring syscall functions are declared in
    `<linux/io_uring.h>`. But they don't actually exist and never existed.
    This is causing confusion for people who read the manpage. Let's just
    implement them in liburing so they exist.
    
    This also allows the user to invoke io_uring syscalls directly instead
    of using the full liburing provided setup.
    
    v2:
      - Use consistent argument types.
      - Separate syscall declarations in liburing.h with a blank line.
      - Remove unused include in syscall.c.
    
    Link: https://github.com/axboe/liburing/pull/646#issuecomment-1229639532
    Reviewed-by: Caleb Sander <csander@purestorage.com>
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>
    Link: https://lore.kernel.org/r/20220830005122.885209-3-ammar.faizi@intel.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- src/include/liburing.h
+++ src/include/liburing.h
@@ -204,6 +204,18 @@ int io_uring_register_notifications(struct io_uring *ring, unsigned nr,
 int io_uring_unregister_notifications(struct io_uring *ring);
 
 /*
+ * io_uring syscalls.
+ */
+int io_uring_enter(unsigned int fd, unsigned int to_submit,
+		   unsigned int min_complete, unsigned int flags, sigset_t *sig);
+int io_uring_enter2(unsigned int fd, unsigned int to_submit,
+		    unsigned int min_complete, unsigned int flags,
+		    sigset_t *sig, size_t sz);
+int io_uring_setup(unsigned int entries, struct io_uring_params *p);
+int io_uring_register(unsigned int fd, unsigned int opcode, const void *arg,
+		      unsigned int nr_args);
+
+/*
  * Helper for the peek/wait single cqe functions. Exported because of that,
  * but probably shouldn't be used directly in an application.
  */
