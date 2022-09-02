6341145b6fe368d123ced2ba9857b3105673f644
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2022-09-02 14:14:55 +0700

    t/poll-link: Don't brute force the port number
    Don't brute force the port number, use `t_bind_ephemeral_port()`,
    much simpler and reliable for choosing a port number that is not
    in use.
    
    v2:
      - While in there, fix variable placements (put them at the top).
    
    Cc: Dylan Yudaken <dylany@fb.com>
    Cc: Facebook Kernel Team <kernel-team@fb.com>
    Cc: Pavel Begunkov <asml.silence@gmail.com>
    Reviewed-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
    Tested-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>
    Link: https://lore.kernel.org/r/20220902071153.3168814-3-ammar.faizi@intel.com
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- test/poll-link.c
+++ test/poll-link.c
@@ -13,6 +13,7 @@
 #include <poll.h>
 #include <arpa/inet.h>
 
+#include "helpers.h"
 #include "liburing.h"
 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
@@ -50,14 +51,14 @@ struct data {
 
 static void *send_thread(void *arg)
 {
+	struct sockaddr_in addr;
 	struct data *data = arg;
+	int s0;
 
 	wait_for_var(&recv_thread_ready);
 
-	int s0 = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	s0 = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
 	assert(s0 != -1);
-
-	struct sockaddr_in addr;
 
 	addr.sin_family = AF_INET;
 	addr.sin_port = data->port;
@@ -72,6 +73,7 @@ static void *send_thread(void *arg)
 
 void *recv_thread(void *arg)
 {
+	struct sockaddr_in addr = { };
 	struct data *data = arg;
 	struct io_uring_sqe *sqe;
 	struct io_uring ring;
@@ -89,27 +91,17 @@ void *recv_thread(void *arg)
 	ret = setsockopt(s0, SOL_SOCKET, SO_REUSEADDR, &val, sizeof(val));
 	assert(ret != -1);
 
-	struct sockaddr_in addr;
-
 	addr.sin_family = AF_INET;
 	data->addr = inet_addr("127.0.0.1");
 	addr.sin_addr.s_addr = data->addr;
 
-	i = 0;
-	do {
-		data->port = htons(1025 + (rand() % 64510));
-		addr.sin_port = data->port;
-
-		if (bind(s0, (struct sockaddr*)&addr, sizeof(addr)) != -1)
-			break;
-	} while (++i < 100);
-
-	if (i >= 100) {
-		fprintf(stderr, "Can't find good port, skipped\n");
+	if (t_bind_ephemeral_port(s0, &addr)) {
+		perror("bind");
 		data->stop = 1;
 		signal_var(&recv_thread_ready);
-		goto out;
+		goto err;
 	}
+	data->port = addr.sin_port;
 
 	ret = listen(s0, 128);
 	assert(ret != -1);
@@ -158,7 +150,6 @@ void *recv_thread(void *arg)
 		io_uring_cqe_seen(&ring, cqe);
 	}
 
-out:
 	signal_var(&recv_thread_done);
 	close(s0);
 	io_uring_queue_exit(&ring);
