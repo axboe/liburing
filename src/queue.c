axboe/liburing

commit e9209a3c6053a94b34919e14553ea9a43a5a5f7c

!SQPOLL setups don't need acquire/release semantics on SQ ring updates
If we're not using SQPOLL, then we know there will be a system call
between the SQ ktail writes or khead reads. This means we don't need
acquire/release semantics on the SQ ring, so make them dependent on
how the ring was setup.

Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/queue.c
@@ -195,7 +195,10 @@ unsigned __io_uring_flush_sq(struct io_uring *ring)
 		/*
 		 * Ensure kernel sees the SQE updates before the tail update.
 		 */
-		io_uring_smp_store_release(sq->ktail, tail);
+		if (!(ring->flags & IORING_SETUP_SQPOLL))
+			io_uring_smp_store_release(sq->ktail, tail);
+		else
+			IO_URING_WRITE_ONCE(*sq->ktail, tail);
 	}
 	/*
 	 * This _may_ look problematic, as we're not supposed to be reading

src/include/liburing.h
@@ -1240,12 +1240,15 @@ static inline int io_uring_wait_cqe(struct io_uring *ring,
 static inline struct io_uring_sqe *_io_uring_get_sqe(struct io_uring *ring)
 {
 	struct io_uring_sq *sq = &ring->sq;
-	unsigned int head = io_uring_smp_load_acquire(sq->khead);
-	unsigned int next = sq->sqe_tail + 1;
+	unsigned int head, next = sq->sqe_tail + 1;
 	int shift = 0;
 
 	if (ring->flags & IORING_SETUP_SQE128)
 		shift = 1;
+	if (!(ring->flags & IORING_SETUP_SQPOLL))
+		head = IO_URING_READ_ONCE(*sq->khead);
+	else
+		head = io_uring_smp_load_acquire(sq->khead);
 
 	if (next - head <= sq->ring_entries) {
 		struct io_uring_sqe *sqe;

commit cc017021ed50626291a5b38a3e05f2e68f75021a

src/queue: fix reversed check for SQPOLL
A recent commit tried to get the memory semantics correct with SQPOLL,
but had one of the conditions reversed! Fix it up so that we don't use
acquire/release semantics UNLESS we're using SQPOLL, which is the only
case that matters.

Fixes: e9209a3c6053 ("!SQPOLL setups don't need acquire/release semantics on SQ ring updates")
Link: https://github.com/axboe/liburing/issues/694
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/queue.c
@@ -196,9 +196,9 @@ unsigned __io_uring_flush_sq(struct io_uring *ring)
 		 * Ensure kernel sees the SQE updates before the tail update.
 		 */
 		if (!(ring->flags & IORING_SETUP_SQPOLL))
-			io_uring_smp_store_release(sq->ktail, tail);
-		else
 			IO_URING_WRITE_ONCE(*sq->ktail, tail);
+		else
+			io_uring_smp_store_release(sq->ktail, tail);
 	}
 	/*
 	 * This _may_ look problematic, as we're not supposed to be reading