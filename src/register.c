3b25a80ecbf15730ac7eefde800270ddb85e4a8c
Stefan Roesch <shr@devkernel.io>
2023-04-25 11:20:51 -0700

    liburing: add api to set napi busy poll settings
    This adds two functions to manage the napi busy poll settings:
    - io_uring_register_napi
    - io_uring_unregister_napi
    
    Signed-off-by: Stefan Roesch <shr@devkernel.io>
    Reviewed-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>
    Link: https://lore.kernel.org/r/20230425182054.2826621-2-shr@devkernel.io
    [axboe: fix nr_args == 0 for register call, as per Olivier Langlois]
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- src/register.c
+++ src/register.c
@@ -356,3 +356,15 @@ int io_uring_register_file_alloc_range(struct io_uring *ring,
 
 	return do_register(ring, IORING_REGISTER_FILE_ALLOC_RANGE, &range, 0);
 }
+
+int io_uring_register_napi(struct io_uring *ring, struct io_uring_napi *napi)
+{
+	return __sys_io_uring_register(ring->ring_fd,
+				IORING_REGISTER_NAPI, napi, 1);
+}
+
+int io_uring_unregister_napi(struct io_uring *ring, struct io_uring_napi *napi)
+{
+	return __sys_io_uring_register(ring->ring_fd,
+				IORING_UNREGISTER_NAPI, napi, 1);
+}
