axboe/liburing

commit 3db49cb6c6e8f7c1da94c4d67176feb0b8477761

Support using IORING_REGISTER_USE_REGISTERED_RING
Use it if the kernel supports IORING_FEAT_REG_REG_RING and the user
registered the ring fd.

Signed-off-by: Josh Triplett <josh@joshtriplett.org>

src/register.c
@@ -11,6 +11,11 @@
 static inline int do_register(struct io_uring *ring, unsigned int opcode,
 			      const void *arg, unsigned int nr_args)
 {
+	if (ring->features & IORING_FEAT_REG_REG_RING
+	    && ring->int_flags & INT_FLAG_REG_RING) {
+		opcode |= IORING_REGISTER_USE_REGISTERED_RING;
+		return __sys_io_uring_register(ring->enter_ring_fd, opcode, arg, nr_args);
+	}
 	return __sys_io_uring_register(ring->ring_fd, opcode, arg, nr_args);
 }
 

man/io_uring_register.2
@@ -31,6 +31,14 @@ application memory, greatly reducing per-I/O overhead.
 .I fd
 is the file descriptor returned by a call to
 .BR io_uring_setup (2).
+If
+.I opcode
+has the flag
+.B IORING_REGISTER_USE_REGISTERED_RING
+ored into it,
+.I fd
+is instead the index of a registered ring fd.
+
 .I opcode
 can be one of:
 

man/io_uring_setup.2
@@ -467,6 +467,13 @@ are used with open or accept, then file assignment needs to happen post
 execution of that SQE. If this flag is set, then the kernel will defer
 file assignment until execution of a given request is started. Available since
 kernel 5.17.
+.TP
+.B IORING_FEAT_REG_REG_RING
+If this flag is set, then io_uring supports calling
+.BR io_uring_register (2)
+using a registered ring fd, via
+.BR IORING_REGISTER_USE_REGISTERED_RING .
+Available since kernel 6.xx.
 
 .PP
 The rest of the fields in the

src/include/liburing/io_uring.h
@@ -472,6 +472,7 @@ struct io_uring_params {
 #define IORING_FEAT_RSRC_TAGS		(1U << 10)
 #define IORING_FEAT_CQE_SKIP		(1U << 11)
 #define IORING_FEAT_LINKED_FILE		(1U << 12)
+#define IORING_FEAT_REG_REG_RING	(1U << 13)
 
 /*
  * io_uring_register(2) opcodes and arguments
@@ -519,7 +520,10 @@ enum {
 	IORING_REGISTER_FILE_ALLOC_RANGE	= 25,
 
 	/* this goes last */
-	IORING_REGISTER_LAST
+	IORING_REGISTER_LAST,
+
+	/* flag added to the opcode to use a registered ring fd */
+	IORING_REGISTER_USE_REGISTERED_RING	= 1U << 31
 };
 
 /* io-wq worker categories */

commit 7d49e22c763c33aeed399796b6188e234bb4e471

src/register: always use ring->enter_ring_fd in do_register()
liburing sets up the ring with ->ring_fd and ->enter_ring_fd being
the same value, so that we don't need these kinds of checks in the fast
path. do_register() isn't a fast path, but let's clean this up and just
use ring->enter_ring_fd unconditionally. If the ring fd itself has been
registered, ->enter_ring_fd will have been updated to the right direct
index.

Fixes: 3db49cb6c6e8 ("Support using IORING_REGISTER_USE_REGISTERED_RING")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/register.c
@@ -11,11 +11,10 @@
 static inline int do_register(struct io_uring *ring, unsigned int opcode,
 			      const void *arg, unsigned int nr_args)
 {
-	if (ring->int_flags & INT_FLAG_REG_REG_RING) {
+	if (ring->int_flags & INT_FLAG_REG_REG_RING)
 		opcode |= IORING_REGISTER_USE_REGISTERED_RING;
-		return __sys_io_uring_register(ring->enter_ring_fd, opcode, arg, nr_args);
-	}
-	return __sys_io_uring_register(ring->ring_fd, opcode, arg, nr_args);
+
+	return __sys_io_uring_register(ring->enter_ring_fd, opcode, arg, nr_args);
 }
 
 int io_uring_register_buffers_update_tag(struct io_uring *ring, unsigned off,