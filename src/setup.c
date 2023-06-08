axboe/liburing

commit 5ae313d1851df1b3108bc85b79fadc84409ca357

src: use __uring_memset() consistently
Something funky is going on with aarc64 and memset, resulting in
having it as an undefined symbol in the final libraries. Killing
string.h and not using memset at all seems to do the trick, though
not sure what is going on here just yet.

Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/setup.c
@@ -99,7 +99,7 @@ __cold int io_uring_queue_mmap(int fd, struct io_uring_params *p,
 {
 	int ret;
 
-	memset(ring, 0, sizeof(*ring));
+	__uring_memset(ring, 0, sizeof(*ring));
 	ret = io_uring_mmap(fd, p, &ring->sq, &ring->cq);
 	if (!ret) {
 		ring->flags = p->flags;
@@ -183,7 +183,7 @@ __cold int io_uring_queue_init(unsigned entries, struct io_uring *ring,
 {
 	struct io_uring_params p;
 
-	memset(&p, 0, sizeof(p));
+	__uring_memset(&p, 0, sizeof(p));
 	p.flags = flags;
 
 	return io_uring_queue_init_params(entries, ring, &p);
@@ -220,7 +220,7 @@ __cold struct io_uring_probe *io_uring_get_probe_ring(struct io_uring *ring)
 	probe = malloc(len);
 	if (!probe)
 		return NULL;
-	memset(probe, 0, len);
+	__uring_memset(probe, 0, len);
 
 	r = io_uring_register_probe(ring, probe, 256);
 	if (r >= 0)
@@ -306,11 +306,13 @@ static size_t rings_size(struct io_uring_params *p, unsigned entries,
 __cold ssize_t io_uring_mlock_size_params(unsigned entries,
 					  struct io_uring_params *p)
 {
-	struct io_uring_params lp = { };
+	struct io_uring_params lp;
 	struct io_uring ring;
 	unsigned cq_entries;
 	long page_size;
 	ssize_t ret;
+
+	__uring_memset(&lp, 0, sizeof(lp));
 
 	/*
 	 * We only really use this inited ring to see if the kernel is newer
@@ -365,8 +367,10 @@ __cold ssize_t io_uring_mlock_size_params(unsigned entries,
  */
 __cold ssize_t io_uring_mlock_size(unsigned entries, unsigned flags)
 {
-	struct io_uring_params p = { .flags = flags, };
+	struct io_uring_params p;
 
+	__uring_memset(&p, 0, sizeof(p));
+	p.flags = flags;
 	return io_uring_mlock_size_params(entries, &p);
 }
 
@@ -375,12 +379,13 @@ static struct io_uring_buf_ring *br_setup(struct io_uring *ring,
 					  unsigned int nentries, int bgid,
 					  unsigned int flags, int *ret)
 {
-	struct io_uring_buf_reg reg = { };
 	struct io_uring_buf_ring *br;
+	struct io_uring_buf_reg reg;
 	size_t ring_size;
 	off_t off;
 	int lret;
 
+	__uring_memset(&reg, 0, sizeof(reg));
 	reg.ring_entries = nentries;
 	reg.bgid = bgid;
 	reg.flags = IOU_PBUF_RING_MMAP;
@@ -408,11 +413,12 @@ static struct io_uring_buf_ring *br_setup(struct io_uring *ring,
 					  unsigned int nentries, int bgid,
 					  unsigned int flags, int *ret)
 {
-	struct io_uring_buf_reg reg = { };
 	struct io_uring_buf_ring *br;
+	struct io_uring_buf_reg reg;
 	size_t ring_size;
 	int lret;
 
+	__uring_memset(&reg, 0, sizeof(reg));
 	ring_size = nentries * sizeof(struct io_uring_buf);
 	br = __sys_mmap(NULL, ring_size, PROT_READ | PROT_WRITE,
 			MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);

src/lib.h
@@ -3,7 +3,6 @@
 #define LIBURING_LIB_H
 
 #include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 
 #if defined(__x86_64__) || defined(__i386__)

commit 310829920d9d0bf15ee2aa22fec71b9bf0931ebd

src: use memset() as the symbol
Previous commit failed to build on !nolibc, breaking archs there...
That was silly, let's get it fixed up.

Fixes: 5ae313d1851d ("src: use __uring_memset() consistently")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/setup.c
@@ -99,7 +99,7 @@ __cold int io_uring_queue_mmap(int fd, struct io_uring_params *p,
 {
 	int ret;
 
-	__uring_memset(ring, 0, sizeof(*ring));
+	memset(ring, 0, sizeof(*ring));
 	ret = io_uring_mmap(fd, p, &ring->sq, &ring->cq);
 	if (!ret) {
 		ring->flags = p->flags;
@@ -183,7 +183,7 @@ __cold int io_uring_queue_init(unsigned entries, struct io_uring *ring,
 {
 	struct io_uring_params p;
 
-	__uring_memset(&p, 0, sizeof(p));
+	memset(&p, 0, sizeof(p));
 	p.flags = flags;
 
 	return io_uring_queue_init_params(entries, ring, &p);
@@ -220,7 +220,7 @@ __cold struct io_uring_probe *io_uring_get_probe_ring(struct io_uring *ring)
 	probe = malloc(len);
 	if (!probe)
 		return NULL;
-	__uring_memset(probe, 0, len);
+	memset(probe, 0, len);
 
 	r = io_uring_register_probe(ring, probe, 256);
 	if (r >= 0)
@@ -312,7 +312,7 @@ __cold ssize_t io_uring_mlock_size_params(unsigned entries,
 	long page_size;
 	ssize_t ret;
 
-	__uring_memset(&lp, 0, sizeof(lp));
+	memset(&lp, 0, sizeof(lp));
 
 	/*
 	 * We only really use this inited ring to see if the kernel is newer
@@ -369,7 +369,7 @@ __cold ssize_t io_uring_mlock_size(unsigned entries, unsigned flags)
 {
 	struct io_uring_params p;
 
-	__uring_memset(&p, 0, sizeof(p));
+	memset(&p, 0, sizeof(p));
 	p.flags = flags;
 	return io_uring_mlock_size_params(entries, &p);
 }
@@ -385,7 +385,7 @@ static struct io_uring_buf_ring *br_setup(struct io_uring *ring,
 	off_t off;
 	int lret;
 
-	__uring_memset(&reg, 0, sizeof(reg));
+	memset(&reg, 0, sizeof(reg));
 	reg.ring_entries = nentries;
 	reg.bgid = bgid;
 	reg.flags = IOU_PBUF_RING_MMAP;
@@ -418,7 +418,7 @@ static struct io_uring_buf_ring *br_setup(struct io_uring *ring,
 	size_t ring_size;
 	int lret;
 
-	__uring_memset(&reg, 0, sizeof(reg));
+	memset(&reg, 0, sizeof(reg));
 	ring_size = nentries * sizeof(struct io_uring_buf);
 	br = __sys_mmap(NULL, ring_size, PROT_READ | PROT_WRITE,
 			MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);