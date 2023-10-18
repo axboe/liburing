794d756c36c6effb1108c78989790d735fb587fa
Jens Axboe <axboe@kernel.dk>
2023-10-18 08:19:13 -0600

    test/no-mmap-inval: add test case for invalid SQ ring address
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

---
+++ test/no-mmap-inval.c
@@ -0,0 +1,36 @@
+/* SPDX-License-Identifier: MIT */
+/*
+ * Description: test that using SETUP_NO_MMAP with an invalid SQ ring
+ *		address fails.
+ *
+ */
+#include <stdlib.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include "liburing.h"
+#include "helpers.h"
+
+int main(int argc, char *argv[])
+{
+	struct io_uring_params p = {
+		.sq_entries	= 2,
+		.cq_entries	= 4,
+		.flags		= IORING_SETUP_NO_MMAP,
+	};
+	int ret;
+
+	if (argc > 1)
+		return T_EXIT_SKIP;
+
+	p.cq_off.user_addr = (unsigned long long) valloc(8192);
+
+	ret = io_uring_setup(2, &p);
+	if (ret != -EFAULT) {
+		fprintf(stderr, "Got %d, wanted -EFAULT\n", ret);
+		return T_EXIT_FAIL;
+	}
+
+	return T_EXIT_PASS;
+}
