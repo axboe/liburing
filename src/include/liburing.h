axboe/liburing

commit 3797db16928a00fe2caa5719d6bde357493ab653

liburing.h: Populate SQE for {s,g} etsockopt
The current io_uring_prep_cmd_sock() liburing function is not populating
the SQE will all the fields because the kernel implementation was not
ready.

With the integration of the kernel part[1], populate the SQE with the
missing fields (optlen, optval, level and optname). This enables
the usage of this function to prepare commands that executes setsockopt
and getsockopt operations.

[1] Link:  https://lore.kernel.org/all/20231016134750.1381153-1-leitao@debian.org/

Signed-off-by: Breno Leitao <leitao@debian.org>
Reviewed-by: Gabriel Krisman Bertazi <krisman@suse.de>
Link: https://lore.kernel.org/r/20231020133917.953642-3-leitao@debian.org
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/include/liburing.h
@@ -1139,8 +1139,6 @@ IOURINGINLINE void io_uring_prep_socket_direct_alloc(struct io_uring_sqe *sqe,
 	__io_uring_set_target_fixed_file(sqe, IORING_FILE_INDEX_ALLOC - 1);
 }
 
-#define UNUSED(x) (void)(x)
-
 /*
  * Prepare commands for sockets
  */
@@ -1152,13 +1150,12 @@ IOURINGINLINE void io_uring_prep_cmd_sock(struct io_uring_sqe *sqe,
 					  void *optval,
 					  int optlen)
 {
-	/* This will be removed once the get/setsockopt() patches land */
-	UNUSED(optlen);
-	UNUSED(optval);
-	UNUSED(level);
-	UNUSED(optname);
 	io_uring_prep_rw(IORING_OP_URING_CMD, sqe, fd, NULL, 0, 0);
+	sqe->optval = (long long unsigned int)optval;
+	sqe->optname = optname;
+	sqe->optlen = optlen;
 	sqe->cmd_op = cmd_op;
+	sqe->level = level;
 }
 
 IOURINGINLINE void io_uring_prep_waitid(struct io_uring_sqe *sqe,

commit 4cb70b7ebd7056790cc6a48c768209641ca02a50

io_uring_prep_cmd_sock: cast pointer to untptr_t first
For 32-bit archs, cast it to an uintptr_t first to avoid warnings.

Fixes: 3797db16928a ("liburing.h: Populate SQE for {s,g} etsockopt")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/include/liburing.h
@@ -1151,7 +1151,7 @@ IOURINGINLINE void io_uring_prep_cmd_sock(struct io_uring_sqe *sqe,
 					  int optlen)
 {
 	io_uring_prep_rw(IORING_OP_URING_CMD, sqe, fd, NULL, 0, 0);
-	sqe->optval = (long long unsigned int)optval;
+	sqe->optval = (unsigned long) (uintptr_t) optval;
 	sqe->optname = optname;
 	sqe->optlen = optlen;
 	sqe->cmd_op = cmd_op;