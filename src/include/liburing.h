axboe/liburing

commit c4a10701bd63a11aefe2ee6e0e48006382bd4de0

Add combined cq+buf ring advance helper
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/include/liburing.h
@@ -1035,12 +1035,27 @@ static inline void io_uring_buf_ring_add(struct io_uring_buf *buf,
  * io_uring_buf_ring_add() has been called 'count' times to fill in new
  * buffers.
  */
-static inline void io_uring_buf_ring_increment(struct io_uring_buf_ring *br,
-					       int count)
+static inline void io_uring_buf_ring_advance(struct io_uring_buf_ring *br,
+					     int count)
 {
 	unsigned short new_tail = br->tail + count;
 
 	io_uring_smp_store_release(&br->tail, new_tail);
+}
+
+/*
+ * Make 'count' new buffers visible to the kernel while at the same time
+ * advancing the CQ ring seen entries. This can be used when the application
+ * is using ring provided buffers and returns buffers while processing CQEs,
+ * avoiding an extra atomic when needing to increment both the CQ ring and
+ * the ring buffer index at the same time.
+ */
+static inline void io_uring_buf_ring_cq_advance(struct io_uring *ring,
+						struct io_uring_buf_ring *br,
+						int count)
+{
+	br->tail += count;
+	io_uring_cq_advance(ring, count);
 }
 
 #ifndef LIBURING_INTERNAL

test/send_recvmsg.c
@@ -201,7 +201,7 @@ static void *recv_fn(void *data)
 			br = ptr;
 			io_uring_buf_ring_add(&br->bufs[0], buf, sizeof(buf),
 						BUF_BID);
-			io_uring_buf_ring_increment(br, 1);
+			io_uring_buf_ring_advance(br, 1);
 		} else {
 			struct io_uring_sqe *sqe;
 			struct io_uring_cqe *cqe;
@@ -366,7 +366,6 @@ int main(int argc, char *argv[])
 	if (argc > 1)
 		return 0;
 
-goto foo;
 	ret = test(0, 0, 0, 1, 0);
 	if (ret) {
 		fprintf(stderr, "send_recvmsg 0 0 0 1 0 failed\n");
@@ -433,7 +432,6 @@ goto foo;
 		return 1;
 	}
 
-foo:
 	ret = test(0, 1, 0, 1, 1);
 	if (ret) {
 		fprintf(stderr, "send_recvmsg async 0 1 0 1 1 failed\n");

commit c44319a6bfb9953b430325227a94136690c19c33

io_uring_buf_ring_cq_advance: ensure tail store is ordered with buffers
We skipped the store ordering for the bundled helper that updates both
the CQ and buffer rings, but this isn't always safe - if an in-kernel
consumer (such as an SQPOLL or io-wq thread) is simultaneously grabbing
a buffer that was just provided prior to calling this helper AND we're
on a weakly ordered architecture that can reorder stores, then it is
possible to have a tiny window of a few cycles where a consumer can find
the tail updated but the buffer not updated.

On x86/x86-64 this can never happen, for example, and for performant use
cases, it's not possible either as they would not use SQPOLL or io-wq
offload and hence the thread itself will always be both the producer and
the consumer. But it is theoretically possible, so we should cover all
the bases.

This should still work better than updating the CQ and buffer ring
separately, only because we can tightly bundle the two ordered stores.
And having them back-to-back will be faster than having other stores (or
loads) in-between.

Fixes: c4a10701bd63 ("Add combined cq+buf ring advance helper")
Link: https://github.com/axboe/liburing/issues/1039
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/include/liburing.h
@@ -1463,7 +1463,7 @@ IOURINGINLINE void __io_uring_buf_ring_cq_advance(struct io_uring *ring,
 						  struct io_uring_buf_ring *br,
 						  int cq_count, int buf_count)
 {
-	br->tail += buf_count;
+	io_uring_buf_ring_advance(br, buf_count);
 	io_uring_cq_advance(ring, cq_count);
 }
 