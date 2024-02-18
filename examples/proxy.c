axboe/liburing

commit 4ef5e451e4bd21c5f5325c9a625fdc1754c0c76c

examples/proxy: fix silly error in grabbing an SQE twice
A previous commit introduced this silly error, which would cause
crashes when we disconnected. Just remove the redundant line, we only
need two SQEs here.

Fixes: b4ddf19d4a78 ("examples/proxy: handle unlikely case of shutdown returning an error")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

examples/proxy.c
@@ -519,7 +519,6 @@ static void queue_shutdown_close(struct io_uring *ring, struct conn *c, int fd)
 	sqe1 = get_sqe(ring);
 	sqe2 = get_sqe(ring);
 
-	sqe1 = get_sqe(ring);
 	io_uring_prep_shutdown(sqe1, fd, SHUT_RDWR);
 	if (fixed_files)
 		sqe1->flags |= IOSQE_FIXED_FILE;

commit 56e9ea5cf2d359b9b2002b3e15e21ca54e5e1e5e

examples/proxy: start opcode numbering from 1
This is a better idea as it can catch unitialized use of user_data,
as was shown by:

4ef5e451e4bd ("examples/proxy: fix silly error in grabbing an SQE twice")

which caused a crash where it should've just been logging an unknown
command type.

It also helped highlight that our initial accept request hardcoded
that the opcode was 0...

Signed-off-by: Jens Axboe <axboe@kernel.dk>

examples/proxy.c
@@ -48,13 +48,13 @@
  * disconnects, we transition to shutdown and then close.
  */
 enum {
-	__ACCEPT	= 0,
-	__SOCK		= 1,
-	__CONNECT	= 2,
-	__RECV		= 3,
-	__SEND		= 4,
-	__SHUTDOWN	= 5,
-	__CLOSE		= 6,
+	__ACCEPT	= 1,
+	__SOCK		= 2,
+	__CONNECT	= 3,
+	__RECV		= 4,
+	__SEND		= 5,
+	__SHUTDOWN	= 6,
+	__CLOSE		= 7,
 };
 
 static int start_bgid = 1;
@@ -1018,7 +1018,7 @@ static int event_loop(struct io_uring *ring, int fd)
 		io_uring_prep_multishot_accept_direct(sqe, fd, NULL, NULL, 0);
 	else
 		io_uring_prep_multishot_accept(sqe, fd, NULL, NULL, 0);
-	__encode_userdata(sqe, 0, 0, 0, 0, fd);
+	__encode_userdata(sqe, 0, __ACCEPT, 0, 0, fd);
 
 	while (1) {
 		struct __kernel_timespec ts = {