axboe/liburing

commit 3b25a80ecbf15730ac7eefde800270ddb85e4a8c

liburing: add api to set napi busy poll settings
This adds two functions to manage the napi busy poll settings:
- io_uring_register_napi
- io_uring_unregister_napi

Signed-off-by: Stefan Roesch <shr@devkernel.io>
Reviewed-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>
Link: https://lore.kernel.org/r/20230425182054.2826621-2-shr@devkernel.io
[axboe: fix nr_args == 0 for register call, as per Olivier Langlois]
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/register.c
@@ -356,3 +356,15 @@ int io_uring_register_file_alloc_range(struct io_uring *ring,
 
 	return do_register(ring, IORING_REGISTER_FILE_ALLOC_RANGE, &range, 0);
 }
+
+int io_uring_register_napi(struct io_uring *ring, struct io_uring_napi *napi)
+{
+	return __sys_io_uring_register(ring->ring_fd,
+				IORING_REGISTER_NAPI, napi, 1);
+}
+
+int io_uring_unregister_napi(struct io_uring *ring, struct io_uring_napi *napi)
+{
+	return __sys_io_uring_register(ring->ring_fd,
+				IORING_UNREGISTER_NAPI, napi, 1);
+}

src/include/liburing.h
@@ -238,6 +238,9 @@ int io_uring_register_sync_cancel(struct io_uring *ring,
 int io_uring_register_file_alloc_range(struct io_uring *ring,
 					unsigned off, unsigned len);
 
+int io_uring_register_napi(struct io_uring *ring, struct io_uring_napi *napi);
+int io_uring_unregister_napi(struct io_uring *ring, struct io_uring_napi *napi);
+
 int io_uring_get_events(struct io_uring *ring);
 int io_uring_submit_and_get_events(struct io_uring *ring);
 

src/include/liburing/io_uring.h
@@ -565,6 +565,10 @@ enum {
 	/* return status information for a buffer group */
 	IORING_REGISTER_PBUF_STATUS		= 26,
 
+	/* set/clear busy poll settings */
+	IORING_REGISTER_NAPI			= 27,
+	IORING_UNREGISTER_NAPI			= 28,
+
 	/* this goes last */
 	IORING_REGISTER_LAST,
 
@@ -696,6 +700,14 @@ struct io_uring_buf_status {
 	__u32	buf_group;	/* input */
 	__u32	head;		/* output */
 	__u32	resv[8];
+};
+
+/* argument for IORING_(UN)REGISTER_NAPI */
+struct io_uring_napi {
+	__u32   busy_poll_to;
+	__u8    prefer_busy_poll;
+	__u8    pad[3];
+	__u64   resv;
 };
 
 /*

src/liburing-ffi.map
@@ -172,6 +172,8 @@ LIBURING_2.4 {
 		io_uring_prep_msg_ring_fd;
 		io_uring_prep_msg_ring_fd_alloc;
 		io_uring_prep_sendto;
+		io_uring_register_napi;
+		io_uring_unregister_napi;
 	local:
 		*;
 };

src/liburing.map
@@ -89,4 +89,6 @@ LIBURING_2.5 {
 LIBURING_2.6 {
 	global:
 		io_uring_buf_ring_head;
+		io_uring_register_napi;
+		io_uring_unregister_napi;
 } LIBURING_2.5;

commit b74bdd035c4c96096f54adc6e8a06785da2055ea

register: use do_register() for NAPI register/unregister
This is the helper that should get used, so that it works regardless
of whether or not the ring fd itself is registered.

Link: https://github.com/axboe/liburing/issues/1133
Fixes: 3b25a80ecbf1 ("liburing: add api to set napi busy poll settings")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/register.c
@@ -359,12 +359,10 @@ int io_uring_register_file_alloc_range(struct io_uring *ring,
 
 int io_uring_register_napi(struct io_uring *ring, struct io_uring_napi *napi)
 {
-	return __sys_io_uring_register(ring->ring_fd,
-				IORING_REGISTER_NAPI, napi, 1);
+	return do_register(ring, IORING_REGISTER_NAPI, napi, 1);
 }
 
 int io_uring_unregister_napi(struct io_uring *ring, struct io_uring_napi *napi)
 {
-	return __sys_io_uring_register(ring->ring_fd,
-				IORING_UNREGISTER_NAPI, napi, 1);
+	return do_register(ring, IORING_UNREGISTER_NAPI, napi, 1);
 }