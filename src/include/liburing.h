7d515bb6a639efc64849223e179ea5d54ceaad0b
Gabriel Krisman Bertazi <krisman@suse.de>
2024-07-23 19:17:30 -0400

    liburing: Add helper to prepare IORING_OP_LISTEN command
    Signed-off-by: Gabriel Krisman Bertazi <krisman@suse.de>
    Link: https://lore.kernel.org/r/20240723231733.31884-3-krisman@suse.de
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- src/include/liburing.h
+++ src/include/liburing.h
@@ -676,6 +676,12 @@ IOURINGINLINE void io_uring_prep_bind(struct io_uring_sqe *sqe, int fd,
 	io_uring_prep_rw(IORING_OP_BIND, sqe, fd, addr, 0, addrlen);
 }
 
+IOURINGINLINE void io_uring_prep_listen(struct io_uring_sqe *sqe, int fd,
+				      int backlog)
+{
+	io_uring_prep_rw(IORING_OP_LISTEN, sqe, fd, 0, backlog, 0);
+}
+
 IOURINGINLINE void io_uring_prep_files_update(struct io_uring_sqe *sqe,
 					      int *fds, unsigned nr_fds,
 					      int offset)
