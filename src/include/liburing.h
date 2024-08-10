axboe/liburing

commit f1a3d813c2f41a7ba32bf929719d84c948933935

Add initialization function of io_uring_sqe
Use io_uring_initialize_sqe to initialize SQEs at the end of
io_uring_get_sqe call. Move initialization to an earlier stage is
helpful as users are not forced to set relevent SQE members after they
call io_uring_prep_*.

Fixes: https://github.com/axboe/liburing/issues/1126
Signed-off-by: gxu <georgexu420@gmail.com>

src/include/liburing.h
@@ -375,23 +375,27 @@ IOURINGINLINE void __io_uring_set_target_fixed_file(struct io_uring_sqe *sqe,
 	sqe->file_index = file_index + 1;
 }
 
-IOURINGINLINE void io_uring_prep_rw(int op, struct io_uring_sqe *sqe, int fd,
-				    const void *addr, unsigned len,
-				    __u64 offset)
+IOURINGINLINE void io_uring_initialize_sqe(struct io_uring_sqe *sqe) 
 {
-	sqe->opcode = (__u8) op;
 	sqe->flags = 0;
 	sqe->ioprio = 0;
-	sqe->fd = fd;
-	sqe->off = offset;
-	sqe->addr = (unsigned long) addr;
-	sqe->len = len;
 	sqe->rw_flags = 0;
 	sqe->buf_index = 0;
 	sqe->personality = 0;
 	sqe->file_index = 0;
 	sqe->addr3 = 0;
 	sqe->__pad2[0] = 0;
+}
+
+IOURINGINLINE void io_uring_prep_rw(int op, struct io_uring_sqe *sqe, int fd,
+				    const void *addr, unsigned len,
+				    __u64 offset)
+{
+	sqe->opcode = (__u8) op;
+	sqe->fd = fd;
+	sqe->off = offset;
+	sqe->addr = (unsigned long) addr;
+	sqe->len = len;
 }
 
 /*
@@ -727,7 +731,6 @@ IOURINGINLINE void io_uring_prep_read_multishot(struct io_uring_sqe *sqe,
 	io_uring_prep_rw(IORING_OP_READ_MULTISHOT, sqe, fd, NULL, nbytes,
 			 offset);
 	sqe->buf_group = buf_group;
-	sqe->flags |= IOSQE_BUFFER_SELECT;
 }
 
 IOURINGINLINE void io_uring_prep_write(struct io_uring_sqe *sqe, int fd,
@@ -1423,6 +1426,7 @@ IOURINGINLINE struct io_uring_sqe *_io_uring_get_sqe(struct io_uring *ring)
 
 		sqe = &sq->sqes[(sq->sqe_tail & sq->ring_mask) << shift];
 		sq->sqe_tail = next;
+		io_uring_initialize_sqe(sqe);
 		return sqe;
 	}
 

commit 60f4adc2c2f1114aeb61ab2d13113a71ea7d705c

Ensure io_uring_prep_read_multishot() sets IOSQE_BUFFER_SELECT
A previous commit removed this for some unknown reason, which is broken
and broke our internal test case as well. That went unnoticed for a
while...

Fixes: f1a3d813c2f4 ("Add initialization function of io_uring_sqe")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/include/liburing.h
@@ -375,7 +375,7 @@ IOURINGINLINE void __io_uring_set_target_fixed_file(struct io_uring_sqe *sqe,
 	sqe->file_index = file_index + 1;
 }
 
-IOURINGINLINE void io_uring_initialize_sqe(struct io_uring_sqe *sqe) 
+IOURINGINLINE void io_uring_initialize_sqe(struct io_uring_sqe *sqe)
 {
 	sqe->flags = 0;
 	sqe->ioprio = 0;
@@ -744,6 +744,7 @@ IOURINGINLINE void io_uring_prep_read_multishot(struct io_uring_sqe *sqe,
 	io_uring_prep_rw(IORING_OP_READ_MULTISHOT, sqe, fd, NULL, nbytes,
 			 offset);
 	sqe->buf_group = buf_group;
+	sqe->flags = IOSQE_BUFFER_SELECT;
 }
 
 IOURINGINLINE void io_uring_prep_write(struct io_uring_sqe *sqe, int fd,