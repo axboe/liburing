a79113633b3caa5aaa380cb55ff19c25636315dd
Jens Axboe <axboe@kernel.dk>
2024-08-30 09:09:41 -0600

    test/unlink: test rmdir
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- test/unlink.c
+++ test/unlink.c
@@ -13,6 +13,55 @@
 
 #include "liburing.h"
 
+static int test_rmdir(struct io_uring *ring)
+{
+	struct io_uring_cqe *cqe;
+	struct io_uring_sqe *sqe;
+	char buf[32];
+	int ret;
+
+	sprintf(buf, ".tmp.dir.%d", getpid());
+	if (mkdir(buf, 0755) < 0) {
+		perror("mkdir");
+		return 1;
+	}
+
+	sqe = io_uring_get_sqe(ring);
+	if (!sqe) {
+		fprintf(stderr, "get sqe failed\n");
+		goto err;
+	}
+	io_uring_prep_unlink(sqe, buf, AT_REMOVEDIR);
+
+	ret = io_uring_submit(ring);
+	if (ret <= 0) {
+		fprintf(stderr, "sqe submit failed: %d\n", ret);
+		goto err;
+	}
+
+	ret = io_uring_wait_cqe(ring, &cqe);
+	if (ret < 0) {
+		fprintf(stderr, "wait completion %d\n", ret);
+		goto err;
+	}
+	ret = cqe->res;
+	io_uring_cqe_seen(ring, cqe);
+
+	if (!ret) {
+		struct stat sb;
+
+		if (!stat(buf, &sb)) {
+			fprintf(stderr, "dir unlinked but still there\n");
+			goto err;
+		}
+	}
+	unlink(buf);
+	return ret;
+err:
+	unlink(buf);
+	return 1;
+}
+
 static int test_unlink_badaddr(struct io_uring *ring)
 {
 	const char *old = (const char *) (uintptr_t) 0x1234;
@@ -44,7 +93,6 @@ static int test_unlink_badaddr(struct io_uring *ring)
 err:
 	return 1;
 }
-
 
 static int test_unlink(struct io_uring *ring, const char *old)
 {
@@ -141,6 +189,12 @@ int main(int argc, char *argv[])
 	ret = test_unlink_badaddr(&ring);
 	if (ret != -EFAULT) {
 		fprintf(stderr, "badaddr unlink got %s\n", strerror(-ret));
+		return 1;
+	}
+
+	ret = test_rmdir(&ring);
+	if (ret) {
+		fprintf(stderr, "rmdir failed: %s\n", strerror(-ret));
 		return 1;
 	}
 
