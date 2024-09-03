axboe/liburing

commit 835ff249f982aea46fe24b33aa990d71783947f7

Define _GNU_SOURCE in liburing.h, if not already set
Rather than require applications set it to get some of the
exported function definitions, just set it in liburing.h. At least then
we keep it as a private scope for our definitions, rather than impose
it on applications as a whole.

Fixes: 6f8b3481cf51 ("Don't set _GNU_SOURCE in pkg-config cflags")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/include/liburing.h
@@ -2,6 +2,10 @@
 #ifndef LIB_URING_H
 #define LIB_URING_H
 
+#ifndef _GNU_SOURCE
+#define _GNU_SOURCE
+#endif
+
 #include <sys/socket.h>
 #include <sys/stat.h>
 #include <sys/uio.h>
@@ -232,10 +236,8 @@ int io_uring_register_restrictions(struct io_uring *ring,
 				   unsigned int nr_res);
 int io_uring_enable_rings(struct io_uring *ring);
 int __io_uring_sqring_wait(struct io_uring *ring);
-#ifdef _GNU_SOURCE
 int io_uring_register_iowq_aff(struct io_uring *ring, size_t cpusz,
 				const cpu_set_t *mask);
-#endif
 int io_uring_unregister_iowq_aff(struct io_uring *ring);
 int io_uring_register_iowq_max_workers(struct io_uring *ring,
 				       unsigned int *values);
@@ -1261,13 +1263,11 @@ IOURINGINLINE void io_uring_prep_fixed_fd_install(struct io_uring_sqe *sqe,
 	sqe->install_fd_flags = flags;
 }
 
-#ifdef _GNU_SOURCE
 IOURINGINLINE void io_uring_prep_ftruncate(struct io_uring_sqe *sqe,
 				       int fd, loff_t len)
 {
 	io_uring_prep_rw(IORING_OP_FTRUNCATE, sqe, fd, 0, 0, len);
 }
-#endif
 
 /*
  * Returns number of unconsumed (if SQPOLL) or unsubmitted entries exist in

man/io_uring_prep_ftruncate.3
@@ -25,12 +25,6 @@ that should get truncated to the length indicated by the
 .I len
 argument.
 
-Applications must define
-.B _GNU_SOURCE
-to obtain the definition of this helper, as
-.I loff_t
-will not be defined without it.
-
 .SH RETURN VALUE
 None
 .SH ERRORS

man/io_uring_register_iowq_aff.3
@@ -38,12 +38,6 @@ For unregistration,
 .BR io_uring_unregister_iowq_aff (3)
 may be called to restore CPU affinities to the default.
 
-Applications must define
-.B _GNU_SOURCE
-to obtain the definition of this helper, as
-.I cpu_set_t
-will not be defined without it.
-
 .SH RETURN VALUE
 Returns
 .B 0

commit 7689d3ac08b7ce675783e78fb3ab037f3f7ef2fb

Final (hopefully) _GNU_SOURCE hack
Let's undef it at the end of liburing.h, if we had to set it. Add a
comment as to why all of this is (supposedly) necessary. Once a new
major release (3.x) is done, this should all go away. For now, we have
the define hack.

Fixes: 835ff249f982 ("Define _GNU_SOURCE in liburing.h, if not already set")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/include/liburing.h
@@ -2,8 +2,17 @@
 #ifndef LIB_URING_H
 #define LIB_URING_H
 
+/*
+ * Define _GNU_SOURCE so we get cpu_set_t and loff_t. Pretty ugly and
+ * really should be up to the application to do so, however there's already
+ * been a release where it unfortunately leaked out of liburing, so better
+ * play it safe rather risk breaking compilation for people. If _GNU_SOURCE
+ * needs defining, undef it at the end of this header, guarded by
+ * LIBURING_UNDEF_GNU_SOURCE.
+ */
 #ifndef _GNU_SOURCE
 #define _GNU_SOURCE
+#define LIBURING_UNDEF_GNU_SOURCE
 #endif
 
 #include <sys/socket.h>
@@ -1601,6 +1610,10 @@ bool io_uring_check_version(int major, int minor);
 
 #ifdef IOURINGINLINE
 #undef IOURINGINLINE
+#endif
+
+#ifdef LIBURING_UNDEF_GNU_SOURCE
+#undef _GNU_SOURCE
 #endif
 
 #endif