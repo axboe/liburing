460c1b4ad78146183ab8f9e184361022fcaef6eb
Jens Axboe <axboe@kernel.dk>
2024-12-30 09:45:22 -0700

    src/setup: use correct sqe size for unmapping
    For some reason only one path actually cares about the SQE size in
    io_uring_queue_exit(). Move the code into the generic case, and use the
    sqe_size variable for both cases.
    
    Otherwise it's unmapping less than what was mapped, if SQE128 is used.
    This can hold the fd pinned, and hence defer release of the ring until
    the task exits (or randomly unmaps the remaining range, which likely
    will never happen as liburing should've done that).
    
    Link: https://github.com/axboe/liburing/issues/1310
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- src/setup.c
+++ src/setup.c
@@ -430,18 +430,17 @@ __cold void io_uring_queue_exit(struct io_uring *ring)
 {
 	struct io_uring_sq *sq = &ring->sq;
 	struct io_uring_cq *cq = &ring->cq;
-	size_t sqe_size;
+	size_t sqe_size = sizeof(struct io_uring_sqe);
+
+	if (ring->flags & IORING_SETUP_SQE128)
+		sqe_size <<= 1;
 
 	if (!sq->ring_sz && !(ring->int_flags & INT_FLAG_APP_MEM)) {
-		sqe_size = sizeof(struct io_uring_sqe);
-		if (ring->flags & IORING_SETUP_SQE128)
-			sqe_size += 64;
 		__sys_munmap(sq->sqes, sqe_size * sq->ring_entries);
 		io_uring_unmap_rings(sq, cq);
 	} else {
 		if (!(ring->int_flags & INT_FLAG_APP_MEM)) {
-			__sys_munmap(sq->sqes,
-				*sq->kring_entries * sizeof(struct io_uring_sqe));
+			__sys_munmap(sq->sqes, *sq->kring_entries * sqe_size);
 			io_uring_unmap_rings(sq, cq);
 		}
 	}
