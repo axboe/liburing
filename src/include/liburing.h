99dbb397550f7318a0c14900e4435795602f028c
Caleb Sander Mateos <csander@purestorage.com>
2025-04-10 17:05:55 -0600

    syscall: update io_uring_enter2() signature
    Since kernel 5.11, io_uring_enter2() accepts a IORING_ENTER_EXT_ARG flag
    allowing a struct io_uring_getevents_arg * to be passed in place of the
    sigset_t * argument. Kernel 6.12 adds the IORING_ENTER_EXT_ARG_REG flag,
    which interprets the argument as an offset into a region of
    struct io_uring_reg_wait's registered with IORING_REGISTER_CQWAIT_REG.
    
    Kernel 5.11 was released over 4 years ago, so it seems more accurate for
    the argument type to be a void * rather than a sigset_t *. Change the
    argument type and rename it from "sig" to "arg" to match the man page.
    Using void * removes the need for a confusing pointer cast for callers
    passing struct io_uring_getevents_arg *.
    
    Signed-off-by: Caleb Sander Mateos <csander@purestorage.com>

--- src/include/liburing.h
+++ src/include/liburing.h
@@ -291,7 +291,7 @@ int io_uring_enter(unsigned int fd, unsigned int to_submit,
 		   unsigned int min_complete, unsigned int flags, sigset_t *sig);
 int io_uring_enter2(unsigned int fd, unsigned int to_submit,
 		    unsigned int min_complete, unsigned int flags,
-		    sigset_t *sig, size_t sz);
+		    void *arg, size_t sz);
 int io_uring_setup(unsigned int entries, struct io_uring_params *p);
 int io_uring_register(unsigned int fd, unsigned int opcode, const void *arg,
 		      unsigned int nr_args);
