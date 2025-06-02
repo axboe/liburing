axboe/liburing

commit 1c1c806ff4e1d2a122180612d5d75cf648a33211

Add struct io_uring_cqe_iter
Introduce a struct io_uring_cqe_iter to encapsulate the state of the
io_uring_for_each_cqe() iteration, simplifying the macro. Implement
helpers io_uring_cqe_iter_init() to initialize the iterator and
io_uring_cqe_iter_next() to advance it and return the next CQE.

This also provides a FFI for iterating over a ring's CQEs.

Signed-off-by: Caleb Sander Mateos <csander@purestorage.com>

src/include/liburing.h
@@ -332,22 +332,45 @@ int __io_uring_get_cqe(struct io_uring *ring,
 #define io_uring_cqe_index(ring,ptr,mask)				\
 	(((ptr) & (mask)) << io_uring_cqe_shift(ring))
 
+struct io_uring_cqe_iter {
+	const struct io_uring *ring;
+	unsigned head;
+	unsigned tail;
+};
+
+IOURINGINLINE struct io_uring_cqe_iter
+io_uring_cqe_iter_init(const struct io_uring *ring)
+{
+	return (struct io_uring_cqe_iter) {
+		.ring = ring,
+		.head = *ring->cq.khead,
+		/* Acquire ordering ensures tail is loaded before any CQEs */
+		.tail = io_uring_smp_load_acquire(ring->cq.ktail),
+	};
+}
+
+IOURINGINLINE struct io_uring_cqe *
+io_uring_cqe_iter_next(struct io_uring_cqe_iter *iter)
+{
+	const struct io_uring *ring = iter->ring;
+	const struct io_uring_cq *cq = &ring->cq;
+
+	if (iter->head == iter->tail)
+		return NULL;
+
+	return &cq->cqes[io_uring_cqe_index(ring, iter->head++, cq->ring_mask)];
+}
+
 /*
  * NOTE: we should just get rid of the 'head' being passed in here, it doesn't
  * serve a purpose anymore. The below is a bit of a work-around to ensure that
  * the compiler doesn't complain about 'head' being unused (or only written,
  * never read), as we use a local iterator for both the head and tail tracking.
  */
-#define io_uring_for_each_cqe(ring, head, cqe)				\
-	/*								\
-	 * io_uring_smp_load_acquire() enforces the order of tail	\
-	 * and CQE reads.						\
-	 */								\
-	for (__u32 __HEAD__ = (head) = *(ring)->cq.khead,		\
-	     __TAIL__ = io_uring_smp_load_acquire((ring)->cq.ktail);	\
-	     (cqe = ((head) != __TAIL__ ?				\
-	     &(ring)->cq.cqes[io_uring_cqe_index(ring, __HEAD__, (ring)->cq.ring_mask)] : NULL)); \
-	     (head) = ++__HEAD__)
+#define io_uring_for_each_cqe(ring, head, cqe)					\
+	for (struct io_uring_cqe_iter __ITER__ = io_uring_cqe_iter_init(ring);	\
+	     (head) = __ITER__.head, cqe = io_uring_cqe_iter_next(&__ITER__);	\
+	     (void)(head))
 
 /*
  * Must be called after io_uring_for_each_cqe()

commit 6836160ae6ae9fdc20d703de55162bfc97169542

Fix potential compilation error due to io_uring_for_each_cqe macro
In 1c1c806ff4e1 ("Add struct io_uring_cqe_iter") the body of the
io_uring_for_each_cqe macro was changed to use io_uring_cqe_iter and
its 'head' member. However, the '.head' use site in the macro's body
clashes with the macro's 'head' argument. This leads to compilation
errors if the macro is invoked with something else as 'head' as second
argument.

For example, Samba uses the macro as follows:

io_uring_for_each_cqe(&config->uring, cqhead, cqe) {
   …
}

which results in

In file included from ../../source3/modules/vfs_io_uring.c:49:
../../source3/modules/vfs_io_uring.c: In function ‘_vfs_io_uring_queue_run’:
../../source3/modules/vfs_io_uring.c:297:47: error: ‘struct io_uring_cqe_iter’ has no member named ‘cqhead’; did you mean ‘head’?
  297 |         io_uring_for_each_cqe(&config->uring, cqhead, cqe) {

as reported in https://bugs.gentoo.org/956879

Fixes: 1c1c806ff4e1d2a122180612d5d75cf648a33211
Signed-off-by: Florian Schmaus <flo@geekplace.eu>

src/include/liburing.h
@@ -379,15 +379,15 @@ IOURINGINLINE bool io_uring_cqe_iter_next(struct io_uring_cqe_iter *iter,
 }
 
 /*
- * NOTE: we should just get rid of the 'head' being passed in here, it doesn't
+ * NOTE: we should just get rid of the '__head__' being passed in here, it doesn't
  * serve a purpose anymore. The below is a bit of a work-around to ensure that
- * the compiler doesn't complain about 'head' being unused (or only written,
+ * the compiler doesn't complain about '__head__' being unused (or only written,
  * never read), as we use a local iterator for both the head and tail tracking.
  */
-#define io_uring_for_each_cqe(ring, head, cqe)					\
+#define io_uring_for_each_cqe(ring, __head__, cqe)					\
 	for (struct io_uring_cqe_iter __ITER__ = io_uring_cqe_iter_init(ring);	\
-	     (head) = __ITER__.head, io_uring_cqe_iter_next(&__ITER__, &(cqe));	\
-	     (void)(head))
+	     (__head__) = __ITER__.head, io_uring_cqe_iter_next(&__ITER__, &(cqe));	\
+	     (void)(__head__))
 
 /*
  * Must be called after io_uring_for_each_cqe()