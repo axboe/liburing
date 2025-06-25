axboe/liburing

commit 57b8229f17001a9857feda19b055001df626af96

src/sanitize.c: Add compile assertion if IORING_OP_LAST increments
To keep sanitizer.c consistent with all IORING_OP* code, add
a compile time assertion in the location where it should be updated
if the IORING_OP_LAST increments and a sanitizer implementation is
not added.

Replace comma initialization to be consistent with rest.

Link: https://github.com/axboe/liburing/pull/1403#issuecomment-2894426034

Signed-off-by: Daniel Black <daniel@mariadb.org>

src/sanitize.c
@@ -115,10 +115,11 @@ static inline void initialize_sanitize_handlers()
 	sanitize_handlers[IORING_OP_FTRUNCATE] = sanitize_sqe_addr;
 	sanitize_handlers[IORING_OP_BIND] = sanitize_sqe_addr;
 	sanitize_handlers[IORING_OP_LISTEN] = sanitize_sqe_addr;
-	sanitize_handlers[IORING_OP_RECV_ZC] = sanitize_sqe_addr;
-	sanitize_handlers[IORING_OP_EPOLL_WAIT] = sanitize_sqe_addr;
-	sanitize_handlers[IORING_OP_READV_FIXED] = sanitize_sqe_addr;
-	sanitize_handlers[IORING_OP_WRITEV_FIXED] = sanitize_sqe_addr;
+	sanitize_handlers[IORING_OP_RECV_ZC] = sanitize_sqe_addr,
+	sanitize_handlers[IORING_OP_EPOLL_WAIT] = sanitize_sqe_addr,
+	sanitize_handlers[IORING_OP_READV_FIXED] = sanitize_sqe_addr,
+	sanitize_handlers[IORING_OP_WRITEV_FIXED] = sanitize_sqe_addr,
+	_Static_assert(IORING_OP_WRITEV_FIXED + 1 == IORING_OP_LAST, "Need an implementation for all IORING_OP_* codes");
 	sanitize_handlers_initialized = true;
 }
 

commit 875fadb95e742af2a6783199ddfe9ab3275aff25

src/sanitize: get rid of comma based initialization
A previous commit fixed this up, but then mistakenly introduced it again
in an unrelated commit. Fix it for real.

Fixes: 57b8229f1700 ("src/sanitize.c: Add compile assertion if IORING_OP_LAST increments")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/sanitize.c
@@ -115,10 +115,10 @@ static inline void initialize_sanitize_handlers()
 	sanitize_handlers[IORING_OP_FTRUNCATE] = sanitize_sqe_addr;
 	sanitize_handlers[IORING_OP_BIND] = sanitize_sqe_addr;
 	sanitize_handlers[IORING_OP_LISTEN] = sanitize_sqe_addr;
-	sanitize_handlers[IORING_OP_RECV_ZC] = sanitize_sqe_addr,
-	sanitize_handlers[IORING_OP_EPOLL_WAIT] = sanitize_sqe_addr,
-	sanitize_handlers[IORING_OP_READV_FIXED] = sanitize_sqe_addr,
-	sanitize_handlers[IORING_OP_WRITEV_FIXED] = sanitize_sqe_addr,
+	sanitize_handlers[IORING_OP_RECV_ZC] = sanitize_sqe_addr;
+	sanitize_handlers[IORING_OP_EPOLL_WAIT] = sanitize_sqe_addr;
+	sanitize_handlers[IORING_OP_READV_FIXED] = sanitize_sqe_addr;
+	sanitize_handlers[IORING_OP_WRITEV_FIXED] = sanitize_sqe_addr;
 	_Static_assert(IORING_OP_WRITEV_FIXED + 1 == IORING_OP_LAST, "Need an implementation for all IORING_OP_* codes");
 	sanitize_handlers_initialized = true;
 }