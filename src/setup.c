axboe/liburing

commit eda0f432d192413d8fcd6edf4742f0d2d4f53f46

src/setup: track sqes mmap size for correct cleanup
Store the actual mmap size used for sqes in a new sqes_sz field in
struct io_uring_sq. This fixes cleanup paths that were using incorrect
sizes for munmap:

- __io_uring_queue_init_params() used the return value from
  io_uring_alloc_huge() (mem_used) which can differ from the actual
  sqes mmap size, particularly in the huge page case where we round
  up to page boundaries.

- io_uring_queue_exit() used io_uring_sqes_size() which calculates
  based on ring entries, but doesn't account for page rounding in
  the NO_MMAP/huge page allocation path.

Now both io_uring_mmap() and io_uring_alloc_huge() record the actual
mmap size, and cleanup paths use this stored value.

Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/setup.c
@@ -139,7 +139,8 @@ int io_uring_mmap(int fd, struct io_uring_params *p, struct io_uring_sq *sq,
 		}
 	}
 
-	sq->sqes = __sys_mmap(0, params_sqes_size(p, p->sq_entries),
+	sq->sqes_sz = params_sqes_size(p, p->sq_entries);
+	sq->sqes = __sys_mmap(0, sq->sqes_sz,
 			      PROT_READ | PROT_WRITE, MAP_SHARED | MAP_POPULATE,
 			      fd, IORING_OFF_SQES);
 	if (IS_ERR(sq->sqes)) {
@@ -268,6 +269,7 @@ static int io_uring_alloc_huge(unsigned entries, struct io_uring_params *p,
 	}
 
 	sq->sqes = ptr;
+	sq->sqes_sz = sqes_size;
 	if (mem_used <= buf_size) {
 		sq->ring_ptr = (void *) sq->sqes + sqes_mem;
 		/* clear ring sizes, we have just one mmap() to undo */
@@ -331,7 +333,7 @@ int __io_uring_queue_init_params(unsigned entries, struct io_uring *ring,
 	if (fd < 0) {
 		if ((p->flags & IORING_SETUP_NO_MMAP) &&
 		    !(ring->int_flags & INT_FLAG_APP_MEM)) {
-			__sys_munmap(ring->sq.sqes, ret);
+			__sys_munmap(ring->sq.sqes, ring->sq.sqes_sz);
 			io_uring_unmap_rings(&ring->sq, &ring->cq);
 		}
 		return fd;
@@ -446,7 +448,7 @@ __cold void io_uring_queue_exit(struct io_uring *ring)
 	struct io_uring_cq *cq = &ring->cq;
 
 	if (!(ring->int_flags & INT_FLAG_APP_MEM)) {
-		__sys_munmap(sq->sqes, io_uring_sqes_size(ring));
+		__sys_munmap(sq->sqes, sq->sqes_sz);
 		io_uring_unmap_rings(sq, cq);
 	}
 

src/include/liburing.h
@@ -138,7 +138,8 @@ struct io_uring_sq {
 	unsigned ring_mask;
 	unsigned ring_entries;
 
-	unsigned pad[2];
+	unsigned sqes_sz;
+	unsigned pad;
 };
 
 struct io_uring_cq {

commit 11b1bad3f8a9d0deb57f829d548472481125dd83

src/setup: check that size_t -> unsigned int cast is OK
This isn't an actual bug, it's just the CI being overly anal. But
this should make it happy.

Fixes: eda0f432d192 ("src/setup: track sqes mmap size for correct cleanup")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/setup.c
@@ -110,6 +110,7 @@ static size_t params_cq_size(const struct io_uring_params *p, unsigned cqes)
 int io_uring_mmap(int fd, struct io_uring_params *p, struct io_uring_sq *sq,
 		  struct io_uring_cq *cq)
 {
+	size_t sqes_sz;
 	int ret;
 
 	sq->ring_sz = p->sq_off.array + p->sq_entries * sizeof(unsigned);
@@ -139,7 +140,13 @@ int io_uring_mmap(int fd, struct io_uring_params *p, struct io_uring_sq *sq,
 		}
 	}
 
-	sq->sqes_sz = params_sqes_size(p, p->sq_entries);
+	sqes_sz = params_sqes_size(p, p->sq_entries);
+	sq->sqes_sz = sqes_sz;
+	if (sq->sqes_sz != sqes_sz) {
+		ret = -EINVAL;
+		goto err;
+	}
+
 	sq->sqes = __sys_mmap(0, sq->sqes_sz,
 			      PROT_READ | PROT_WRITE, MAP_SHARED | MAP_POPULATE,
 			      fd, IORING_OFF_SQES);