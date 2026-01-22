axboe/liburing

commit 11b1bad3f8a9d0deb57f829d548472481125dd83

src/setup: check that size_t -> unsigned int cast is OK
This isn't an actual bug, it's just the CI being overly anal. But
this should make it happy.

Fixes: eda0f432d192 ("src/setup: track sqes mmap size for correct cleanup")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/setup.c
@@ -110,6 +110,7 @@ static size_t params_cq_size(const struct io_uring_params *p, unsigned cqes)
 int io_uring_mmap(int fd, struct io_uring_params *p, struct io_uring_sq *sq,
 		  struct io_uring_cq *cq)
 {
+	size_t sqes_sz;
 	int ret;
 
 	sq->ring_sz = p->sq_off.array + p->sq_entries * sizeof(unsigned);
@@ -139,7 +140,13 @@ int io_uring_mmap(int fd, struct io_uring_params *p, struct io_uring_sq *sq,
 		}
 	}
 
-	sq->sqes_sz = params_sqes_size(p, p->sq_entries);
+	sqes_sz = params_sqes_size(p, p->sq_entries);
+	sq->sqes_sz = sqes_sz;
+	if (sq->sqes_sz != sqes_sz) {
+		ret = -EINVAL;
+		goto err;
+	}
+
 	sq->sqes = __sys_mmap(0, sq->sqes_sz,
 			      PROT_READ | PROT_WRITE, MAP_SHARED | MAP_POPULATE,
 			      fd, IORING_OFF_SQES);

commit ddf61f1daacea4288606b33f0f2d26fe3eee2234

src/setup: actually make CI happy about the size_t -> unsigned int
Guess we need to explicitly cast this to avoid CI being unhappy
about it, so let's do that...

Fixes: 11b1bad3f8a9 ("src/setup: check that size_t -> unsigned int cast is OK")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/setup.c
@@ -141,7 +141,7 @@ int io_uring_mmap(int fd, struct io_uring_params *p, struct io_uring_sq *sq,
 	}
 
 	sqes_sz = params_sqes_size(p, p->sq_entries);
-	sq->sqes_sz = sqes_sz;
+	sq->sqes_sz = (unsigned int) sqes_sz;
 	if (sq->sqes_sz != sqes_sz) {
 		ret = -EINVAL;
 		goto err;