9b7c673fecf8f6043dbc132cadbf5570769efd65
Jens Axboe <axboe@kernel.dk>
2026-02-13 13:38:37 -0700

    test: fix clang-22 over-eager warnings
    clang-22 reports that:
    
    link-timeout.c:30:35: warning: variable 'ts' is uninitialized
          when passed as a const pointer argument here [-Wuninitialized-const-pointer]
       30 |         io_uring_prep_link_timeout(sqe, &ts, 0);
          |                                          ^~
    ring-query.c:42:35: warning: variable 'op' is uninitialized when
          passed as a const pointer argument here [-Wuninitialized-const-pointer]
       42 |                 .query_data = uring_ptr_to_u64(&op),
          |                                                 ^~
    ring-query.c:82:35: warning: variable 'op' is uninitialized when
          passed as a const pointer argument here [-Wuninitialized-const-pointer]
       82 |                 .query_data = uring_ptr_to_u64(&op),
          |                                                 ^~
    ring-query.c:125:35: warning: variable 'op3' is uninitialized
          when passed as a const pointer argument here [-Wuninitialized-const-pointer]
      125 |                 .query_data = uring_ptr_to_u64(&op3),
          |                                                 ^~~
    ring-query.c:130:35: warning: variable 'op2' is uninitialized
          when passed as a const pointer argument here [-Wuninitialized-const-pointer]
      130 |                 .query_data = uring_ptr_to_u64(&op2),
          |                                                 ^~~
    ring-query.c:136:35: warning: variable 'op1' is uninitialized
          when passed as a const pointer argument here [-Wuninitialized-const-pointer]
      136 |                 .query_data = uring_ptr_to_u64(&op1),
          |                                                 ^~~
    ring-query.c:169:35: warning: variable 'op2' is uninitialized
          when passed as a const pointer argument here [-Wuninitialized-const-pointer]
      169 |                 .query_data = uring_ptr_to_u64(&op2),
          |                                                 ^~~
    ring-query.c:174:35: warning: variable 'op1' is uninitialized
          when passed as a const pointer argument here [-Wuninitialized-const-pointer]
      174 |                 .query_data = uring_ptr_to_u64(&op1),
          |                                                 ^~~
    ring-query.c:181:35: warning: variable 'hdr_self_circular' is
          uninitialized when passed as a const pointer argument here
          [-Wuninitialized-const-pointer]
      181 |                 .next_entry = uring_ptr_to_u64(&hdr_self_circular),
          |                                                 ^~~~~~~~~~~~~~~~~
    ring-query.c:207:35: warning: variable 'op' is uninitialized
          when passed as a const pointer argument here [-Wuninitialized-const-pointer]
      207 |                 .query_data = uring_ptr_to_u64(&op),
          |                                                 ^~
    ring-query.c:240:35: warning: variable 'op' is uninitialized
          when passed as a const pointer argument here [-Wuninitialized-const-pointer]
      240 |                 .query_data = uring_ptr_to_u64(&op),
          |                                                 ^~
    
    which actually doesn't matter - yes the thing pointer to isn't
    initialized yet, but it will be before the pointer it actually used.
    Since this is just tests, initialize everything to 0 first. And for
    hdr_self_circular.next_entry, just initialize it post its own
    initialization.
    
    Reported-by: Christian Mazakas <christian.mazakas@gmail.com>
    Signed-off-by: Jens Axboe <axboe@kernel.dk>

--- test/ring-query.c
+++ test/ring-query.c
@@ -36,7 +36,7 @@ static int io_uring_query(struct io_uring *ring, struct io_uring_query_hdr *arg)
 
 static int test_basic_query(void)
 {
-	struct io_uring_query_opcode op;
+	struct io_uring_query_opcode op = { };
 	struct io_uring_query_hdr hdr = {
 		.query_op = IO_URING_QUERY_OPCODES,
 		.query_data = uring_ptr_to_u64(&op),
@@ -76,7 +76,7 @@ static int test_basic_query(void)
 static int test_invalid(void)
 {
 	int ret;
-	struct io_uring_query_opcode op;
+	struct io_uring_query_opcode op = { };
 	struct io_uring_query_hdr invalid_hdr = {
 		.query_op = -1U,
 		.query_data = uring_ptr_to_u64(&op),
@@ -119,7 +119,7 @@ static int test_invalid(void)
 static int test_chain(void)
 {
 	int ret;
-	struct io_uring_query_opcode op1, op2, op3;
+	struct io_uring_query_opcode op1 = { }, op2 = { }, op3 = { };
 	struct io_uring_query_hdr hdr3 = {
 		.query_op = IO_URING_QUERY_OPCODES,
 		.query_data = uring_ptr_to_u64(&op3),
@@ -163,7 +163,7 @@ static int test_chain(void)
 static int test_chain_loop(void)
 {
 	int ret;
-	struct io_uring_query_opcode op1, op2;
+	struct io_uring_query_opcode op1 = { }, op2 = { };
 	struct io_uring_query_hdr hdr2 = {
 		.query_op = IO_URING_QUERY_OPCODES,
 		.query_data = uring_ptr_to_u64(&op2),
@@ -178,8 +178,9 @@ static int test_chain_loop(void)
 		.query_op = IO_URING_QUERY_OPCODES,
 		.query_data = uring_ptr_to_u64(&op1),
 		.size = sizeof(struct io_uring_query_opcode),
-		.next_entry = uring_ptr_to_u64(&hdr_self_circular),
 	};
+
+	hdr_self_circular.next_entry = uring_ptr_to_u64(&hdr_self_circular);
 
 	hdr1.next_entry = uring_ptr_to_u64(&hdr2);
 	hdr2.next_entry = uring_ptr_to_u64(&hdr1);
@@ -201,7 +202,7 @@ static int test_chain_loop(void)
 static int test_compatibile_shorter(void)
 {
 	int ret;
-	struct io_uring_query_opcode_short op;
+	struct io_uring_query_opcode_short op = { };
 	struct io_uring_query_hdr hdr = {
 		.query_op = IO_URING_QUERY_OPCODES,
 		.query_data = uring_ptr_to_u64(&op),
@@ -234,7 +235,7 @@ static int test_compatibile_shorter(void)
 static int test_compatibile_larger(void)
 {
 	int ret;
-	struct io_uring_query_opcode_large op;
+	struct io_uring_query_opcode_large op = { };
 	struct io_uring_query_hdr hdr = {
 		.query_op = IO_URING_QUERY_OPCODES,
 		.query_data = uring_ptr_to_u64(&op),
