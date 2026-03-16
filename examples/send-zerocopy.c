axboe/liburing

commit 3e4f0534266209b548acf97624a75cb351473473

examples/send-zerocopy: use strncpy() to copy interface name
Odd construct, and clang rightfully complains about it:

send-zerocopy.c:351:48: warning: format string is not a string literal (potentially insecure) [-Wformat-security]
  351 |                 snprintf(ifr.ifr_name, sizeof(ifr.ifr_name), cfg_ifname);
      |                                                              ^~~~~~~~~~
send-zerocopy.c:351:48: note: treat the string as an argument to avoid this
  351 |                 snprintf(ifr.ifr_name, sizeof(ifr.ifr_name), cfg_ifname);
      |                                                              ^
      |                                                              "%s",
1 warning generated.

Fixes: 78d0ed571c13 ("examples/send-zc: option to bind socket to device")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

examples/send-zerocopy.c
@@ -351,7 +351,7 @@ static void do_tx(struct thread_data *td, int domain, int type, int protocol)
 		struct ifreq ifr;
 
 		memset(&ifr, 0, sizeof(ifr));
-		snprintf(ifr.ifr_name, sizeof(ifr.ifr_name), cfg_ifname);
+		strncpy(ifr.ifr_name, cfg_ifname, sizeof(ifr.ifr_name));
 
 		if (setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, &ifr, sizeof(ifr)) < 0)
 			t_error(1, errno, "Binding to device failed\n");

commit b6a45c81fe1dd6d657f0ea96873cf38a69b0a410

examples/send-zerocopy: fix -Wstringop-truncation on ifr.ifr_name
strncpy(ifr.ifr_name, cfg_ifname, sizeof(ifr.ifr_name)) triggers
-Wstringop-truncation because the bound equals the destination size,
so the compiler assumes the result may not be null-terminated. Use
sizeof(ifr.ifr_name) - 1 as the bound so at most 15 bytes are copied.

Fixes: 3e4f05342662 ("examples/send-zerocopy: use strncpy() to copy interface name")
Signed-off-by: Yang Xiuwei <yangxiuwei@kylinos.cn>
Link: https://patch.msgid.link/20260316013621.115939-2-yangxiuwei@kylinos.cn
Signed-off-by: Jens Axboe <axboe@kernel.dk>

examples/send-zerocopy.c
@@ -353,7 +353,7 @@ static void do_tx(struct thread_data *td, int domain, int type, int protocol)
 		struct ifreq ifr;
 
 		memset(&ifr, 0, sizeof(ifr));
-		strncpy(ifr.ifr_name, cfg_ifname, sizeof(ifr.ifr_name));
+		strncpy(ifr.ifr_name, cfg_ifname, sizeof(ifr.ifr_name) - 1);
 
 		if (setsockopt(fd, SOL_SOCKET, SO_BINDTODEVICE, &ifr, sizeof(ifr)) < 0)
 			t_error(1, errno, "Binding to device failed\n");