axboe/liburing

commit 9cecbdb6092c336084bd04d05f1fef601be9f6b7

Add support for IORING_SETUP_CQE_MIXED
This adds core support for mixed sized CQEs in the same CQ ring.
Before this, CQEs were either 16b in size (the normal size), or 32b
if IORING_SETUP_CQE32 was set in the ring initialization. With the
mixed support, a CQE may be either 16b or 32b on the same CQ ring.
If the CQE is 32b in size, then IORING_CQE_F_32 will be set, and
the kernel may post a CQE with IORING_CQE_F_SKIP that should simply
be ignored as it's just a pad filler that is posted when required.

Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/include/liburing.h
@@ -394,6 +394,11 @@ IOURINGINLINE unsigned io_uring_cqe_shift(const struct io_uring *ring)
 	return io_uring_cqe_shift_from_flags(ring->flags);
 }
 
+IOURINGINLINE unsigned io_uring_cqe_nr(const struct io_uring_cqe *cqe)
+{
+	return 1U << !!(cqe->flags & IORING_CQE_F_32);
+}
+
 struct io_uring_cqe_iter {
 	struct io_uring_cqe *cqes;
 	unsigned mask;
@@ -424,6 +429,8 @@ static inline bool io_uring_cqe_iter_next(struct io_uring_cqe_iter *iter,
 		return false;
 
 	*cqe = &iter->cqes[(iter->head++ & iter->mask) << iter->shift];
+	if ((*cqe)->flags & IORING_CQE_F_32)
+		iter->head++;
 	return true;
 }
 
@@ -464,7 +471,7 @@ IOURINGINLINE void io_uring_cqe_seen(struct io_uring *ring,
 	LIBURING_NOEXCEPT
 {
 	if (cqe)
-		io_uring_cq_advance(ring, 1);
+		io_uring_cq_advance(ring, io_uring_cqe_nr(cqe));
 }
 
 /*
@@ -1737,6 +1744,22 @@ IOURINGINLINE int io_uring_wait_cqe_nr(struct io_uring *ring,
 	return __io_uring_get_cqe(ring, cqe_ptr, 0, wait_nr, NULL);
 }
 
+static inline bool io_uring_skip_cqe(struct io_uring *ring,
+				     struct io_uring_cqe *cqe, int *err)
+{
+	if (cqe->flags & IORING_CQE_F_SKIP)
+		goto out;
+	if (ring->features & IORING_FEAT_EXT_ARG)
+		return false;
+	if (cqe->user_data != LIBURING_UDATA_TIMEOUT)
+		return false;
+	if (cqe->res < 0)
+		*err = cqe->res;
+out:
+	io_uring_cq_advance(ring, io_uring_cqe_nr(cqe));
+	return !*err;
+}
+
 /*
  * Internal helper, don't use directly in applications. Use one of the
  * "official" versions of this, io_uring_peek_cqe(), io_uring_wait_cqe(),
@@ -1768,17 +1791,9 @@ static inline int __io_uring_peek_cqe(struct io_uring *ring,
 			break;
 
 		cqe = &ring->cq.cqes[(head & mask) << shift];
-		if (!(ring->features & IORING_FEAT_EXT_ARG) &&
-				cqe->user_data == LIBURING_UDATA_TIMEOUT) {
-			if (cqe->res < 0)
-				err = cqe->res;
-			io_uring_cq_advance(ring, 1);
-			if (!err)
-				continue;
-			cqe = NULL;
-		}
-
-		break;
+		if (!io_uring_skip_cqe(ring, cqe, &err))
+			break;
+		cqe = NULL;
 	} while (1);
 
 	*cqe_ptr = cqe;

man/io_uring.7
@@ -514,6 +514,23 @@ left off. This can only happen if the provided buffer ring has been setup
 with
 .B IOU_PBUF_RING_INC
 to allow for incremental / partial consumption of buffers.
+.TP
+.B IORING_CQE_F_SKIP
+If the ring has been configured with
+.B IORING_SETUP_CQE_MIXED ,
+then CQEs may be posted which has this flag set. This can happen if the ring
+is a single 16b CQE entry away from wrapping, but someone needs to post a 32b
+CQE. As CQEs must be contiguous in memory, a filler/pad CQE needs to get
+posted to allow posting of the 32b CQE. CQEs with this flag set should simply
+be skipped and ignored, they serve no other purpose than to fill a gap in the
+CQ ring.
+.TP
+.B IORING_CQE_F_32
+If the ring has been configured with
+.B IORING_SETUP_CQE_MIXED ,
+this flag is set when the CQE is of the 32b type. This tells the application
+that there's an extra 16b of space in this CQE, and that to get to the next
+CQE the CQ ring must be advanced by twice as much as for a normal 16b CQE.
 .PP
 The general sequence to read completion events off the completion queue is
 as follows:

man/io_uring_setup.2
@@ -278,7 +278,9 @@ If set, io_uring will use 32-byte CQEs rather than the normal 16-byte sized
 variant. This is a requirement for using certain request types, as of 5.19
 only the
 .B IORING_OP_URING_CMD
-passthrough command for NVMe passthrough needs this. Available since 5.19.
+passthrough command for NVMe passthrough needs this. Also see
+.B IORING_SETUP_CQE_MIXED
+for an alternative to this flag. Available since 5.19.
 .TP
 .B IORING_SETUP_SINGLE_ISSUER
 A hint to the kernel that only a single task (or thread) will submit requests, which is
@@ -357,6 +359,34 @@ should not map the array of submission queue entries, and the corresponding
 offset in
 .I struct io_sqring_offsets
 will be set to zero. Available since 6.6.
+
+.TP
+.B IORING_SETUP_CQE_MIXED
+If this flag is set, the ring supports posting both the normal 16b sized CQEs
+as well as the larger 32b CQEs. Normally a ring is setup to only allow posting
+of normal 16b CQEs, or if
+.B IORING_SETUP_CQE32
+has been set, only 32b CQEs. With this flag, the kernel may post both types.
+If a 32b CQE is posted, the CQE
+.I flags
+member will have
+.B IORING_CQE_F_32
+set to indicate that this CQE is of the larger type. The kernel may also set
+.B IORING_CQE_F_SKIP
+in the CQE
+.I flags
+member, in case it needs to post a filler 16b CQE. This will happen if a 32b
+CQE needs posting and the current CQ ring offset is such that the 32b CQE
+would wrap around. Applications should simply skip/ignore such CQEs, they
+contain no information and serve only as placeholder to facilitate advancing
+the CQ ring such that a large CQE can get posted. The use case of the
+.B IORING_SETUP_CQE_MIXED
+flag is to allow more efficient setups of workloads that may post both normal
+and large CQEs. Opcodes that previously required the use of
+.B IORING_SETUP_CQE32
+may now use this flag instead, and potentially save some memory for the CQ
+ring. If used with liburing 2.12 or newer, the handling of variably sized CQEs
+is fully transparent. Available since 6.18.
 
 .PP
 If no flags are specified, the io_uring instance is setup for

src/include/liburing/io_uring.h
@@ -205,6 +205,12 @@ enum io_uring_sqe_flags_bit {
 /* Use hybrid poll in iopoll process */
 #define IORING_SETUP_HYBRID_IOPOLL	(1U << 17)
 
+/*
+ * Allow both 16b and 32b CQEs. If a 32b CQE is posted, it will have
+ * IORING_CQE_F_32 set in cqe->flags.
+ */
+#define IORING_SETUP_CQE_MIXED		(1U << 18)
+
 enum io_uring_op {
 	IORING_OP_NOP,
 	IORING_OP_READV,
@@ -463,12 +469,22 @@ struct io_uring_cqe {
  *			other provided buffer type, all completions with a
  *			buffer passed back is automatically returned to the
  *			application.
+ * IORING_CQE_F_SKIP	If set, then the application/liburing must ignore this
+ *			CQE. It's only purpose is to fill a gap in the ring,
+ *			if a large CQE is attempted posted when the ring has
+ *			just a single small CQE worth of space left before
+ *			wrapping.
+ * IORING_CQE_F_32	If set, this is a 32b/big-cqe posting. Use with rings
+ *			setup in a mixed CQE mode, where both 16b and 32b
+ *			CQEs may be posted to the CQ ring.
  */
 #define IORING_CQE_F_BUFFER		(1U << 0)
 #define IORING_CQE_F_MORE		(1U << 1)
 #define IORING_CQE_F_SOCK_NONEMPTY	(1U << 2)
 #define IORING_CQE_F_NOTIF		(1U << 3)
 #define IORING_CQE_F_BUF_MORE		(1U << 4)
+#define IORING_CQE_F_SKIP		(1U << 5)
+#define IORING_CQE_F_32			(1U << 15)
 
 #define IORING_CQE_BUFFER_SHIFT		16
 

src/liburing-ffi.map
@@ -249,4 +249,5 @@ LIBURING_2.12 {
 	global:
 		io_uring_prep_pipe;
 		io_uring_prep_pipe_direct;
+		io_uring_cqe_nr;
 } LIBURING_2.11;

commit 7e01907b4a9d46da7215269949db66384657e510

liburing.h: don't return stale CQE pointer with timeout error
On kernels without IORING_FEAT_EXT_ARG, when the internal timeout CQE
completes with res < 0, io_uring_skip_cqe() sets the error, advances
the CQ head past the CQE, and returns false. __io_uring_peek_cqe()
would then break out with cqe still pointing at the released slot, and
the get_cqe path stored that to *cqe_ptr - handing the application
-ETIME along with a pointer to a CQE the kernel is free to overwrite.
The pre-refactor code cleared the CQE pointer in this case.

Fixes: 9cecbdb6092c ("Add support for IORING_SETUP_CQE_MIXED")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/include/liburing.h
@@ -1893,8 +1893,16 @@ IOURINGINLINE int __io_uring_peek_cqe(struct io_uring *ring,
 			break;
 
 		cqe = &ring->cq.cqes[(head & mask) << shift];
-		if (!io_uring_skip_cqe(ring, cqe, &err))
+		if (!io_uring_skip_cqe(ring, cqe, &err)) {
+			/*
+			 * If an error was set, the CQE was an internal
+			 * timeout and has already been consumed - don't
+			 * return a pointer to it.
+			 */
+			if (err)
+				cqe = NULL;
 			break;
+		}
 		cqe = NULL;
 	} while (1);
 