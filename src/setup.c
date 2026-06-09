axboe/liburing

commit 6102a6e848b2cea0c0c1e35aecb08473e3395127

setup: add support for IORING_SETUP_NO_MMAP
If this flag is set when io_uring_queue_init_params() is called, then
we'll pre-allocate ring and sqe data before calling into the kernel to
setup the ring. The kernel will then map this pre-allocated data, rather
than allocate its own.

This allows use of huge pages for the backing of the rings and sqes. If
both rings and sqes fit in a single huge page, then we just need one page
for all of it. If they don't fit, then we fall back to one huge page for
the sqes, and one for the rings. This should be improved, we can really
pack things any way we want.

Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/setup.c
@@ -60,8 +60,9 @@ static int get_sq_cq_entries(unsigned entries, struct io_uring_params *p,
 
 static void io_uring_unmap_rings(struct io_uring_sq *sq, struct io_uring_cq *cq)
 {
-	__sys_munmap(sq->ring_ptr, sq->ring_sz);
-	if (cq->ring_ptr && cq->ring_ptr != sq->ring_ptr)
+	if (sq->ring_sz)
+		__sys_munmap(sq->ring_ptr, sq->ring_sz);
+	if (cq->ring_ptr && cq->ring_sz && cq->ring_ptr != sq->ring_ptr)
 		__sys_munmap(cq->ring_ptr, cq->ring_sz);
 }
 
@@ -154,17 +155,8 @@ err:
 __cold int io_uring_queue_mmap(int fd, struct io_uring_params *p,
 			       struct io_uring *ring)
 {
-	int ret;
-
 	memset(ring, 0, sizeof(*ring));
-	ret = io_uring_mmap(fd, p, &ring->sq, &ring->cq);
-	if (!ret) {
-		ring->flags = p->flags;
-		ring->ring_fd = ring->enter_ring_fd = fd;
-		ring->int_flags = 0;
-		return 0;
-	}
-	return ret;
+	return io_uring_mmap(fd, p, &ring->sq, &ring->cq);
 }
 
 /*
@@ -202,6 +194,62 @@ __cold int io_uring_ring_dontfork(struct io_uring *ring)
 	return 0;
 }
 
+/* FIXME */
+static int huge_page_size = 2 * 1024 * 1024;
+
+static int io_uring_alloc_huge(unsigned entries, struct io_uring_params *p,
+			       struct io_uring_sq *sq, struct io_uring_cq *cq)
+{
+	unsigned sq_entries, cq_entries;
+	size_t ring_mem, sqes_mem;
+	void *ptr;
+	int ret;
+
+	ret = get_sq_cq_entries(entries, p, &sq_entries, &cq_entries);
+	if (ret)
+		return ret;
+
+	sqes_mem = sq_entries * sizeof(struct io_uring_sqe);
+	ring_mem = cq_entries * sizeof(struct io_uring_cqe);
+	ring_mem += sq_entries * sizeof(unsigned);
+
+	ptr = __sys_mmap(NULL, huge_page_size, PROT_READ|PROT_WRITE,
+				MAP_SHARED|MAP_ANONYMOUS|MAP_HUGETLB, -1, 0);
+	if (IS_ERR(ptr))
+		return PTR_ERR(ptr);
+
+	sq->sqes = ptr;
+	memset(ptr, 0, huge_page_size);
+	if (sqes_mem + ring_mem <= huge_page_size) {
+		size_t sqes_size = sq_entries * sizeof(struct io_uring_sqe);
+		unsigned long page_size = get_page_size();
+
+		/* everything fits in one huge page */
+		sqes_size = (sqes_size + page_size - 1) & ~(page_size - 1);
+		sq->ring_ptr = (void *) sq->sqes + sqes_size;
+		/* clear ring sizes, we have just one mmap() to undo */
+		cq->ring_sz = 0;
+		sq->ring_sz = 0;
+	} else {
+		ptr = __sys_mmap(NULL, huge_page_size, PROT_READ|PROT_WRITE,
+					MAP_SHARED|MAP_ANONYMOUS|MAP_HUGETLB,
+					-1, 0);
+		if (IS_ERR(ptr)) {
+			__sys_munmap(sq->sqes, huge_page_size);
+			return PTR_ERR(ptr);
+		}
+		memset(ptr, 0, huge_page_size);
+		sq->ring_ptr = ptr;
+		sq->ring_sz = huge_page_size;
+		cq->ring_sz = 0;
+	}
+
+	cq->ring_ptr = (void *) sq->ring_ptr;
+	p->sq_off.user_addr = (unsigned long) sq->sqes;
+	p->cq_off.user_addr = (unsigned long) sq->ring_ptr;
+	return 0;
+}
+
 __cold int io_uring_queue_init_params(unsigned entries, struct io_uring *ring,
 				      struct io_uring_params *p)
 {
@@ -209,14 +257,31 @@ __cold int io_uring_queue_init_params(unsigned entries, struct io_uring *ring,
 	unsigned *sq_array;
 	unsigned sq_entries, index;
 
-	fd = __sys_io_uring_setup(entries, p);
-	if (fd < 0)
-		return fd;
+	memset(ring, 0, sizeof(*ring));
 
-	ret = io_uring_queue_mmap(fd, p, ring);
-	if (ret) {
-		__sys_close(fd);
-		return ret;
+	if (p->flags & IORING_SETUP_NO_MMAP) {
+		ret = io_uring_alloc_huge(entries, p, &ring->sq, &ring->cq);
+		if (ret)
+			return ret;
+	}
+
+	fd = __sys_io_uring_setup(entries, p);
+	if (fd < 0) {
+		if (p->flags & IORING_SETUP_NO_MMAP) {
+			__sys_munmap(ring->sq.sqes, huge_page_size);
+			io_uring_unmap_rings(&ring->sq, &ring->cq);
+		}
+		return fd;
+	}
+
+	if (!(p->flags & IORING_SETUP_NO_MMAP)) {
+		ret = io_uring_queue_mmap(fd, p, ring);
+		if (ret) {
+			__sys_close(fd);
+			return ret;
+		}
+	} else {
+		io_uring_setup_ring_pointers(p, &ring->sq, &ring->cq);
 	}
 
 	/*
@@ -228,6 +293,8 @@ __cold int io_uring_queue_init_params(unsigned entries, struct io_uring *ring,
 		sq_array[index] = index;
 
 	ring->features = p->features;
+	ring->flags = p->flags;
+	ring->ring_fd = ring->enter_ring_fd = fd;
 	return 0;
 }
 
@@ -252,11 +319,17 @@ __cold void io_uring_queue_exit(struct io_uring *ring)
 	struct io_uring_cq *cq = &ring->cq;
 	size_t sqe_size;
 
-	sqe_size = sizeof(struct io_uring_sqe);
-	if (ring->flags & IORING_SETUP_SQE128)
-		sqe_size += 64;
-	__sys_munmap(sq->sqes, sqe_size * sq->ring_entries);
-	io_uring_unmap_rings(sq, cq);
+	if (!sq->ring_sz) {
+		sqe_size = sizeof(struct io_uring_sqe);
+		if (ring->flags & IORING_SETUP_SQE128)
+			sqe_size += 64;
+		__sys_munmap(sq->sqes, sqe_size * sq->ring_entries);
+		io_uring_unmap_rings(sq, cq);
+	} else {
+		__sys_munmap(sq->sqes,
+				*sq->kring_entries * sizeof(struct io_uring_sqe));
+		io_uring_unmap_rings(sq, cq);
+	}
 	/*
 	 * Not strictly required, but frees up the slot we used now rather
 	 * than at process exit time.

commit 435e6b286e40552f5c07f86a098e410c3670b54e

src/setup: restore ring fd and flags init in io_uring_queue_mmap()
Commit 6102a6e848b2 dropped the assignment of ring->flags and the ring
fds from io_uring_queue_mmap(). It's an exported and documented API for
applications that call io_uring_setup(2) themselves, and such users
ended up with a ring that had ring_fd/enter_ring_fd set to 0, where
io_uring_submit() would target stdin and io_uring_queue_exit() would
close fd 0. The internal caller sets the fields up after the call,
hence liburing itself didn't notice.

Restore the assignments, and also set ->features which is needed for
correct CQE handling these days.

Fixes: 6102a6e848b2 ("setup: add support for IORING_SETUP_NO_MMAP")
Signed-off-by: Jens Axboe <axboe@kernel.dk>

src/setup.c
@@ -170,8 +170,16 @@ err:
 __cold int io_uring_queue_mmap(int fd, struct io_uring_params *p,
 			       struct io_uring *ring)
 {
+	int ret;
+
 	memset(ring, 0, sizeof(*ring));
-	return io_uring_mmap(fd, p, &ring->sq, &ring->cq);
+	ret = io_uring_mmap(fd, p, &ring->sq, &ring->cq);
+	if (!ret) {
+		ring->flags = p->flags;
+		ring->features = p->features;
+		ring->ring_fd = ring->enter_ring_fd = fd;
+	}
+	return ret;
 }
 
 static size_t io_uring_sqes_size(const struct io_uring *ring)